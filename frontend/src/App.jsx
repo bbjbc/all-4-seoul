0d98e1230944e510a7e39babe3bdd86d9f3c7de4
bbjbc <jobc4550@gmail.com>
2024-03-22 15:41:39 +0900

    feat: js파일 jsx로 변경

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  const router = createBrowserRouter([{ path: '/', element: <div>Home</div> }]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
