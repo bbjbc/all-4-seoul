dd772e215698a8183d09718b324e81f874bbdb71
bbjbc <jobc4550@gmail.com>
2024-03-27 22:34:34 +0900

    feat: 렌더링 확인을 위한 더미데이터 추가

---
+++ frontend/src/components/places/dummy-data.jsx
@@ -0,0 +1,68 @@
+import image1 from '../../assets/성신여대입구역.jpg';
+import image2 from '../../assets/대림역.jpg';
+import image3 from '../../assets/건대입구역.jpg';
+import image4 from '../../assets/발산역.jpg';
+import image5 from '../../assets/연신내역.jpg';
+import image6 from '../../assets/왕십리역.jpg';
+import image7 from '../../assets/장한평역.jpg';
+import image8 from '../../assets/잠실한강공원.jpg';
+import image9 from '../../assets/경복궁.jpg';
+
+const DummyData = [
+  {
+    id: 1,
+    name: '성신여대입구역',
+    category: '인구밀집지역',
+    images: image1,
+  },
+  {
+    id: 2,
+    name: '대림역',
+    category: '인구밀집지역',
+    images: image2,
+  },
+  {
+    id: 3,
+    name: '건대입구역',
+    category: '인구밀집지역',
+    images: image3,
+  },
+  {
+    id: 4,
+    name: '발산역',
+    category: '인구밀집지역',
+    images: image4,
+  },
+  {
+    id: 5,
+    name: '연신내역',
+    category: '인구밀집지역',
+    images: image5,
+  },
+  {
+    id: 6,
+    name: '왕십리역',
+    category: '인구밀집지역',
+    images: image6,
+  },
+  {
+    id: 7,
+    name: '장한평역',
+    category: '인구밀집지역',
+    images: image7,
+  },
+  {
+    id: 8,
+    name: '잠실한강공원',
+    category: '공원',
+    images: image8,
+  },
+  {
+    id: 9,
+    name: '경복궁',
+    category: '고궁·문화유산',
+    images: image9,
+  },
+];
+
+export default DummyData;
