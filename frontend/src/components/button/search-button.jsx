e50f7f60b6c1f8aa8dc6dd3217b8bc318e62c421
bbjbc <jobc4550@gmail.com>
2024-03-29 00:02:34 +0900

    feat: 모달에서 키스트로크마다 저장된 장소명에 따른 리스트화

--- frontend/src/components/button/search-button.jsx
+++ frontend/src/components/button/search-button.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 import Modal from '../modal/modal';
 import ModalPortal from '../modal/modal-portal';
+import DummyData from '../places/dummy-data';
 
 function SearchButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -12,6 +15,17 @@ function SearchButton() {
 
   const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  const inputChangeHandler = (e) => {
+    setSearchInput(e.target.value);
+    filterData(e.target.value);
+    if (e.target.value === '') setFilteredData([]);
+  };
+
+  const filterData = (input) => {
+    const filtered = DummyData.filter((d) => d.name.includes(input));
+    setFilteredData(filtered);
   };
 
   return (
@@ -26,12 +40,37 @@ function SearchButton() {
       {isModalOpen && (
         <ModalPortal>
           <Modal onClose={closeModal}>
-            <h1 className="mb-4 text-2xl font-bold">원하는 장소 있나요?</h1>
+            <h1 className="mb-4 text-center text-2xl font-bold">
+              원하는 장소 있나요?
+            </h1>
             <input
               type="text"
+              value={searchInput}
+              onChange={inputChangeHandler}
               placeholder="장소를 입력하세요."
-              className="m-2 rounded-md border border-gray-300 p-2 focus:outline-gray-600"
+              className="mb-4 rounded-md border border-gray-300 p-2 focus:outline-gray-600"
             />
+            <div className="max-h-[500px] overflow-y-auto">
+              <ul className="space-y-2">
+                {filteredData.length > 0 ? (
+                  filteredData.map((item) => (
+                    <li
+                      key={item.id}
+                      className="rounded-md p-4 transition-colors duration-300 hover:bg-gray-100"
+                    >
+                      <div>
+                        <h1>{item.name}</h1>
+                        <p className="text-xs text-gray-600">{item.category}</p>
+                      </div>
+                    </li>
+                  ))
+                ) : (
+                  <li className="mt-20 animate-bounce text-center text-gray-600">
+                    검색 결과가 없습니다.
+                  </li>
+                )}
+              </ul>
+            </div>
           </Modal>
         </ModalPortal>
       )}
