0564ebe66d03eeabd221857f7cbd2e0d8750bb69
jae woo Jung <awfjol2008@naver.com>
2024-03-29 11:40:43 +0900

    feat: ParkingLot 엔티티 칼럼 속성 설정 추가

--- backend/src/main/java/com/capstone/all4seoul/place/domain/ParkingLot.java
+++ backend/src/main/java/com/capstone/all4seoul/place/domain/ParkingLot.java
@@ -1,8 +1,21 @@
 package com.capstone.all4seoul.place.domain;
 
+import jakarta.persistence.Column;
+
 public class ParkingLot {
-    private int capacity;
-    private int price;
-    private int dues;
+
+    @Column(name = "capacity")
+    private String capacity;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "time_rates")
+    private String timeRates;
+
+    @Column(name = "add_rates")
+    private String addRates;
+
+    @Column(name = "etc")
     private String etc;
 }
