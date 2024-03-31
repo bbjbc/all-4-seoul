343587e4dcda61750e60d7738b10d3f201d8b84d
bbjbc <jobc4550@gmail.com>
2024-03-31 23:07:10 +0900

    feat: 로그인, 회원가입 폼 컴포넌트 분리

---
+++ frontend/src/components/auth/login-form.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import Input from '../input/input';
+import SubmitButton from '../button/submit-button';
+
+function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className="relative z-10 w-96 animate-slidein rounded-lg bg-white p-8 shadow-lg"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="mb-8 text-center text-3xl font-bold">Login</h2>
+      <Input
+        id="id"
+        label="ID"
+        type="text"
+        placeholder="아이디를 입력해주세요."
+        register={register}
+        errors={errors}
+        errorMessage="아이디를 입력해주세요."
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        placeholder="패스워드를 입력해주세요."
+        register={register}
+        errors={errors}
+        errorMessage="패스워드를 입력해주세요."
+      />
+
+      <p className="p-3">
+        회원이 아니신가요? &nbsp;
+        <Link
+          to="/signup"
+          className="text-blue-500 underline hover:text-blue-800"
+        >
+          회원가입
+        </Link>
+        하세요!
+      </p>
+      <SubmitButton text="로그인" />
+    </form>
+  );
+}
+
+export default LoginForm;
---
+++ frontend/src/components/auth/signup-form.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import IdInput from '../../components/input/id-input';
+import PasswordInput from '../../components/input/password-input';
+import PasswordConfirmInput from '../../components/input/password-confirm-input';
+import NameInput from '../../components/input/name-input';
+import DateInput from '../../components/input/date-input';
+import Option from '../../components/input/option';
+import SubmitButton from '../../components/button/submit-button';
+
+function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    getValues,
+  } = useForm({ mode: 'onBlur' });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className="relative z-10 max-h-[80vh] w-96 animate-slidein overflow-y-auto scroll-smooth rounded-lg bg-white p-8 shadow-lg"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="mb-8 text-center text-3xl font-bold">Signup</h2>
+      <IdInput register={register} errors={errors} />
+      <PasswordInput register={register} errors={errors} />
+      <PasswordConfirmInput
+        register={register}
+        errors={errors}
+        getValues={getValues}
+      />
+      <NameInput register={register} errors={errors} />
+      <DateInput
+        id="birth"
+        label="Birth"
+        placeholder="생년월일을 입력해주세요."
+        control={control}
+        defaultValue={new Date()}
+      />
+      <Option label="MBTI" id="mbti" register={register}>
+        <option value="ISTJ">ISTJ</option>
+        <option value="ISFJ">ISFJ</option>
+        <option value="INFJ">INFJ</option>
+        <option value="INTJ">INTJ</option>
+        <option value="ISTP">ISTP</option>
+        <option value="ISFP">ISFP</option>
+        <option value="INFP">INFP</option>
+        <option value="INTP">INTP</option>
+        <option value="ESTP">ESTP</option>
+        <option value="ESFP">ESFP</option>
+        <option value="ENFP">ENFP</option>
+        <option value="ENTP">ENTP</option>
+        <option value="ESTJ">ESTJ</option>
+        <option value="ESFJ">ESFJ</option>
+        <option value="ENFJ">ENFJ</option>
+        <option value="ENTJ">ENTJ</option>
+      </Option>
+      <Option label="Gender" id="gender" register={register}>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </Option>
+
+      <p className="p-3">
+        이미 회원이신가요? &nbsp;
+        <Link
+          to="/login"
+          className="text-blue-500 underline hover:text-blue-800"
+        >
+          로그인
+        </Link>
+        하세요!
+      </p>
+      <SubmitButton text="회원가입" />
+    </form>
+  );
+}
+
+export default SignupForm;
