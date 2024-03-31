0363847ed7c561a9d95a1d6be2e744eb66afd896
fbehddn <tom0611@naver.com>
2024-03-31 20:37:49 +0900

    feat: Review 레포지토리 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/review/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.capstone.all4seoul.review.repository;
+
+import com.capstone.all4seoul.review.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    //유저 아이디로 리뷰 리스트 조회
+    List<Review> findByUserLoginId(String loginId);
+
+    //장소 이름으로 리뷰 리스트 조회
+    List<Review> findByPlaceName(String placeId);
+}
