6b7386549484201ddf382fafff07e212053fc660
bbjbc <jobc4550@gmail.com>
2024-04-01 19:26:30 +0900

    style: 카테고리 클릭 후 focus 효과 추가로 사용자 경험 증대

--- frontend/src/components/button/category-button.jsx
+++ frontend/src/components/button/category-button.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function CategoryButton({ icon: Icon, text, onClick }) {
   return (
     <button
-      className="flex items-center rounded-full border-2 border-gray-600 bg-zinc-100 px-3 py-1 text-center shadow-xl hover:bg-slate-200"
+      className="flex items-center rounded-full border-2 border-gray-600 bg-zinc-100 px-3 py-1 text-center shadow-xl hover:bg-slate-200 focus:bg-yellow-100"
       onClick={onClick}
     >
       <Icon size={20} />
