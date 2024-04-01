d9a0d6f1b7a82a69b82871dccb829430bb16f601
bbjbc <jobc4550@gmail.com>
2024-04-01 19:15:35 +0900

    feat: 다양한 이미지 마커 기능 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import KakaoMap from '../components/map/kakao-map';
-import MapCategory from '../components/map/map-category';
 
 function HomePage() {
   return (
     <>
-      <MapCategory />
       <KakaoMap />
     </>
   );
