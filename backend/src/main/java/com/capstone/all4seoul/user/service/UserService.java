bb63848912ec30a6ef1f47c933f49d9445c0bc45
fbehddn <tom0611@naver.com>
2024-04-02 18:34:34 +0900

    feat: UserService 생성

---
+++ backend/src/main/java/com/capstone/all4seoul/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.capstone.all4seoul.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
