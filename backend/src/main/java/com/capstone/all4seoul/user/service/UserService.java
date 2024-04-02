6740a47784cd584a1c574a893e52a31ba7436272
fbehddn <tom0611@naver.com>
2024-04-02 18:35:21 +0900

    feat: 회원가입 기능 구현

--- backend/src/main/java/com/capstone/all4seoul/user/service/UserService.java
+++ backend/src/main/java/com/capstone/all4seoul/user/service/UserService.java
@@ -1,9 +1,26 @@
 package com.capstone.all4seoul.user.service;
 
+import com.capstone.all4seoul.user.domain.User;
+import com.capstone.all4seoul.user.dto.UserDto;
+import com.capstone.all4seoul.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public void joinUser(UserDto userDto) {
+        User user = User.createUser(userDto);
+
+        // 사용자 생성 전 중복 로그인 ID 확인
+        if (userRepository.findByLoginId(userDto.getLoginId()) != null) {
+            throw new IllegalArgumentException("이미 사용 중인 로그인 아이디입니다.");
+        }
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            throw new RuntimeException("사용자 생성 중 오류가 발생했습니다.", e);
+        }
+    }
 }
