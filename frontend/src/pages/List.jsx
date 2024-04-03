82b061a1ef19a6505829be66037612a06b0a193d
bbjbc <jobc4550@gmail.com>
2024-04-03 23:54:00 +0900

    feat: 리스트로 볼 때 다량의 데이터 중 찾기 어려울 사용자 경험을 고려한 입력에 따른 장소 렌더링 추가

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import PlaceGrid from '../components/places/place-grid';
 import ListData from '../data/list-data';
 import CategoryBar from '../components/places/category-bar';
+import PlaceSearchInput from '../components/places/place-search-input';
 
 function ListPage() {
   const allCategories = Array.from(
     new Set(['전체보기', ...ListData.map((item) => item.category)]),
   );
   const [selectedCategory, setSelectedCategory] = useState('전체보기');
+  const [searchWord, setSearchWord] = useState('');
 
   const changeCategoryHandler = (newCategory) => {
     setSelectedCategory(newCategory);
@@ -16,8 +18,16 @@ function ListPage() {
 
   const filteredPlaces =
     selectedCategory === '전체보기'
-      ? ListData
-      : ListData.filter((item) => item.category === selectedCategory);
+      ? ListData.filter((item) => item.name.includes(searchWord))
+      : ListData.filter(
+          (item) =>
+            item.category === selectedCategory &&
+            item.name.includes(searchWord),
+        );
+
+  const searchHandler = (input) => {
+    setSearchWord(input);
+  };
 
   return (
     <div className="px-14 py-32">
@@ -26,6 +36,7 @@ function ListPage() {
         selectedCategory={selectedCategory}
         changeCategory={changeCategoryHandler}
       />
+      <PlaceSearchInput onSearchChange={searchHandler} />
       <PlaceGrid places={filteredPlaces} />
     </div>
   );
