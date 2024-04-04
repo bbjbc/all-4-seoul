3bad3db1a89c592840eecb21a5e6edec407bd594
bbjbc <jobc4550@gmail.com>
2024-04-04 17:28:30 +0900

    feat: 리스트 페이지 무한 스크롤 구현

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useInView } from 'react-intersection-observer';
 
 import PlaceGrid from '../components/places/place-grid';
 import ListData from '../data/list-data';
 import CategoryBar from '../components/places/category-bar';
 import PlaceSearchInput from '../components/places/place-search-input';
+import LoadingSpinner from '../components/button/loading-spinner';
 
 function ListPage() {
   const allCategories = Array.from(
@@ -11,23 +14,49 @@ function ListPage() {
   );
   const [selectedCategory, setSelectedCategory] = useState('전체보기');
   const [searchWord, setSearchWord] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [places, setPlaces] = useState([]);
+  const pageSize = 10;
+
+  const loadPlaces = () => {
+    setLoading(true);
+    const filteredPlaces =
+      selectedCategory === '전체보기'
+        ? ListData.filter((item) => item.name.includes(searchWord))
+        : ListData.filter(
+            (item) =>
+              item.category === selectedCategory &&
+              item.name.includes(searchWord),
+          );
+    const newPlaces = filteredPlaces.slice(0, page * pageSize);
+    setPlaces(newPlaces);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadPlaces();
+  }, [selectedCategory, searchWord, page, pageSize, loadPlaces]);
 
   const changeCategoryHandler = (newCategory) => {
     setSelectedCategory(newCategory);
+    setPage(1);
   };
-
-  const filteredPlaces =
-    selectedCategory === '전체보기'
-      ? ListData.filter((item) => item.name.includes(searchWord))
-      : ListData.filter(
-          (item) =>
-            item.category === selectedCategory &&
-            item.name.includes(searchWord),
-        );
 
   const searchHandler = (input) => {
     setSearchWord(input);
+    setPage(1);
   };
+
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (inView && !loading) {
+      setPage((prev) => prev + 1);
+    }
+  }, [inView, loading]);
 
   return (
     <div className="px-14 py-32">
@@ -37,7 +66,9 @@ function ListPage() {
         changeCategory={changeCategoryHandler}
       />
       <PlaceSearchInput onSearchChange={searchHandler} />
-      <PlaceGrid places={filteredPlaces} />
+      <PlaceGrid places={places} />
+      {loading && <LoadingSpinner />}
+      <div ref={ref}></div>
     </div>
   );
 }
