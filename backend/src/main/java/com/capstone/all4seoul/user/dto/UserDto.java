9b2e46de153e85f510421f7a3de4180ba03f6105
fbehddn <tom0611@naver.com>
2024-04-05 10:21:25 +0900

    feat: 회원가입 요청 DTO 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/user/dto/UserDto.java
@@ -0,0 +1,39 @@
+package com.capstone.all4seoul.user.dto;
+
+import com.capstone.all4seoul.user.domain.Gender;
+import com.capstone.all4seoul.user.domain.Mbti;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UserDto {
+    @NotBlank(message = "로그인 ID를 입력해주세요.")
+    private String loginId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String loginPassword;
+
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String username;
+
+    @NotNull(message = "생년월일을 입력해주세요.")
+    @Past(message = "유효한 날짜를 입력해주세요.")
+    private LocalDate birth;
+
+    @NotNull(message = "MBTI를 입력해주세요.")
+    private Mbti mbti;
+
+    @NotNull(message = "성별을 선택해주세요.")
+    private Gender gender;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    private String nickName;
+
+}
+
