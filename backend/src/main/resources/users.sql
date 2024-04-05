99a9400b89c4f997825e021946efdbfc60740047
jae woo Jung <awfjol2008@naver.com>
2024-04-05 13:54:25 +0900

    feat: DDL 및 더미데이터 관련 DML SQL 추가

---
+++ backend/src/main/resources/users.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users;
+
+create table users
+(
+    user_id        INT,
+    login_id       VARCHAR(50),
+    login_password VARCHAR(50),
+    nam            VARCHAR(50),
+    birth          DATE,
+    mbti           VARCHAR(50),
+    gender         VARCHAR(50),
+    nickname       VARCHAR(50)
+);
+insert into users (user_id, login_id, login_password, nam, birth, mbti, gender, nickname)
+values (1, 'tdawkes0@is.gd', 'zI4`(Q,udf40}WJ', 'Avigdor Meigh', '2023-08-16 22:21:22', 'ISTP', 'Male',
+        'Tabor Dawkes');
+insert into users (user_id, login_id, login_password, nam, birth, mbti, gender, nickname)
+values (2, 'lbittany1@ftc.gov', 'jS1#`89eGg8%OesC', 'Thaddus McCullouch', '2023-10-30 05:45:08', 'ISFP',
+        'Male', 'Laurie Bittany');
