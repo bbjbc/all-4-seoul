b63a2b0f82b982a318740782e8c5f677022bebbd
bbjbc <jobc4550@gmail.com>
2024-04-05 14:15:04 +0900

    test: 카테고리에 따른 마커 띄우기 & 마우스 idle 이벤트에 따른 마커 갱신

--- frontend/src/components/map/kakao-map.jsx
+++ frontend/src/components/map/kakao-map.jsx
@@ -1,124 +1,235 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { Map } from 'react-kakao-maps-sdk';
-import { MapMarker } from 'react-kakao-maps-sdk';
+const MapComponent = () => {
+  useEffect(() => {
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(37.555946, 126.972317),
+      level: 5,
+    };
 
-import MapCategory from './map-category';
+    const map = new window.kakao.maps.Map(
+      document.getElementById('map'),
+      mapOption,
+    );
+    const ps = new window.kakao.maps.services.Places(map);
 
-function KakaoMap() {
-  // 마커이미지의 주소입니다. 스프라이트 이미지 입니다
-  const markerImageSrc =
-    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/category.png';
+    const searchPlaces = () => {
+      if (!currCategory) {
+        return;
+      }
 
-  const imageSize = new window.kakao.maps.Size(27, 28);
-  const spriteSize = { width: 36, height: 98 };
+      placeOverlay.setMap(null);
+      removeMarker();
 
-  // 커피숍 마커가 표시될 좌표 배열입니다
-  const coffeePositions = [
-    { lat: 37.499590490909185, lng: 127.0263723554437 },
-    { lat: 37.499427948430814, lng: 127.02794423197847 },
-    { lat: 37.498553760499505, lng: 127.02882598822454 },
-    { lat: 37.497625593121384, lng: 127.02935713582038 },
-    { lat: 37.49646391248451, lng: 127.02675574250912 },
-    { lat: 37.49629291770947, lng: 127.02587362608637 },
-    { lat: 37.49754540521486, lng: 127.02546694890695 },
-  ];
-  const coffeeOrigin = { x: 10, y: 0 };
+      ps.categorySearch(currCategory, placesSearchCB, { useMapBounds: true });
+    };
 
-  // 편의점 마커가 표시될 좌표 배열입니다
-  const storePositions = [
-    { lat: 37.497535461505684, lng: 127.02948149502778 },
-    { lat: 37.49671536281186, lng: 127.03020491448352 },
-    { lat: 37.496201943633714, lng: 127.02959405469642 },
-    { lat: 37.49640072567703, lng: 127.02726459882308 },
-    { lat: 37.49640098874988, lng: 127.02609983175294 },
-    { lat: 37.49932849491523, lng: 127.02935780247945 },
-    { lat: 37.49996818951873, lng: 127.02943721562295 },
-  ];
-  const storeOrigin = { x: 10, y: 36 };
+    window.kakao.maps.event.addListener(map, 'idle', searchPlaces);
 
-  // 주차장 마커가 표시될 좌표 배열입니다
-  const carparkPositions = [
-    { lat: 37.49966168796031, lng: 127.03007039430118 },
-    { lat: 37.499463762912974, lng: 127.0288828824399 },
-    { lat: 37.49896834100913, lng: 127.02833986892401 },
-    { lat: 37.49893267508434, lng: 127.02673400572665 },
-    { lat: 37.49872543597439, lng: 127.02676785815386 },
-    { lat: 37.49813096097184, lng: 127.02591949495914 },
-    { lat: 37.497680616783086, lng: 127.02518427952202 },
-  ];
-  const carparkOrigin = { x: 10, y: 72 };
+    const placeOverlay = new window.kakao.maps.CustomOverlay({ zIndex: 1 });
+    const contentNode = document.createElement('div');
+    contentNode.className = 'placeinfo_wrap';
 
-  const [selectedCategory, setSelectedCategory] = useState();
+    const addEventHandle = (target, type, callback) => {
+      if (target.addEventListener) {
+        target.addEventListener(type, callback);
+      } else {
+        target.attachEvent('on' + type, callback);
+      }
+    };
+    addEventHandle(
+      contentNode,
+      'mousedown',
+      window.kakao.maps.event.preventMap,
+    );
+    addEventHandle(
+      contentNode,
+      'touchstart',
+      window.kakao.maps.event.preventMap,
+    );
+
+    const placesSearchCB = (data, status) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        displayPlaces(data);
+      } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
+        // Handle zero result case
+      } else if (status === window.kakao.maps.services.Status.ERROR) {
+        // Handle error case
+      }
+    };
+
+    const displayPlaces = (places) => {
+      const order = document
+        .getElementById(currCategory)
+        .getAttribute('data-order');
+
+      for (let i = 0; i < places.length; i++) {
+        const marker = addMarker(
+          new window.kakao.maps.LatLng(places[i].y, places[i].x),
+          order,
+        );
+
+        (function (marker, place) {
+          window.kakao.maps.event.addListener(marker, 'click', function () {
+            displayPlaceInfo(place);
+          });
+        })(marker, places[i]);
+      }
+    };
+
+    const addMarker = (position, order) => {
+      const imageSrc =
+        'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png';
+      const imageSize = new window.kakao.maps.Size(27, 28);
+      const imgOptions = {
+        spriteSize: new window.kakao.maps.Size(72, 208),
+        spriteOrigin: new window.kakao.maps.Point(46, order * 36),
+        offset: new window.kakao.maps.Point(11, 28),
+      };
+      const markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imgOptions,
+      );
+      const marker = new window.kakao.maps.Marker({
+        position: position,
+        image: markerImage,
+      });
+
+      marker.setMap(map);
+      markers.push(marker);
+
+      return marker;
+    };
+
+    const removeMarker = () => {
+      for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = [];
+    };
+
+    const displayPlaceInfo = (place) => {
+      let content =
+        '<div class="placeinfo">' +
+        '   <a class="title" href="' +
+        place.place_url +
+        '" target="_blank" title="' +
+        place.place_name +
+        '">' +
+        place.place_name +
+        '</a>';
+
+      if (place.road_address_name) {
+        content +=
+          '    <span title="' +
+          place.road_address_name +
+          '">' +
+          place.road_address_name +
+          '</span>' +
+          '  <span class="jibun" title="' +
+          place.address_name +
+          '">(지번 : ' +
+          place.address_name +
+          ')</span>';
+      } else {
+        content +=
+          '    <span title="' +
+          place.address_name +
+          '">' +
+          place.address_name +
+          '</span>';
+      }
+
+      content +=
+        '    <span class="tel">' +
+        place.phone +
+        '</span>' +
+        '</div>' +
+        '<div class="after"></div>';
+
+      contentNode.innerHTML = content;
+      placeOverlay.setPosition(new window.kakao.maps.LatLng(place.y, place.x));
+      placeOverlay.setMap(map);
+    };
+
+    const addCategoryClickEvent = () => {
+      const category = document.getElementById('category');
+      const children = category.children;
+
+      for (let i = 0; i < children.length; i++) {
+        children[i].onclick = onClickCategory;
+      }
+    };
+
+    const onClickCategory = (event) => {
+      const id = event.target.id;
+      const className = event.target.className;
+
+      placeOverlay.setMap(null);
+
+      if (className === 'on') {
+        currCategory = '';
+        changeCategoryClass();
+        removeMarker();
+      } else {
+        currCategory = id;
+        changeCategoryClass(event.target);
+        searchPlaces();
+      }
+    };
+
+    const changeCategoryClass = (el) => {
+      const category = document.getElementById('category');
+      const children = category.children;
+
+      for (let i = 0; i < children.length; i++) {
+        children[i].className = '';
+      }
+
+      if (el) {
+        el.className = 'on';
+      }
+    };
+
+    let markers = [];
+    let currCategory = '';
+
+    addCategoryClickEvent();
+  }, []);
 
   return (
-    <>
-      <div id="mapwrap">
-        <Map // 지도를 표시할 Container
-          id={`map`}
-          center={{
-            // 지도의 중심좌표
-            lat: 37.498004414546934,
-            lng: 127.02770621963765,
-          }}
-          style={{
-            // 지도의 크기
-            width: '100%',
-            height: '100vh',
-          }}
-          level={3} // 지도의 확대 레벨
-        >
-          {selectedCategory === 'coffee' &&
-            coffeePositions.map((position) => (
-              <MapMarker
-                key={`coffee-${position.lat},${position.lng}`}
-                position={position}
-                image={{
-                  src: markerImageSrc,
-                  size: imageSize,
-                  options: {
-                    spriteSize: spriteSize,
-                    spriteOrigin: coffeeOrigin,
-                  },
-                }}
-              />
-            ))}
-          {selectedCategory === 'store' &&
-            storePositions.map((position) => (
-              <MapMarker
-                key={`store-${position.lat},${position.lng}`}
-                position={position}
-                image={{
-                  src: markerImageSrc,
-                  size: imageSize,
-                  options: {
-                    spriteSize: spriteSize,
-                    spriteOrigin: storeOrigin,
-                  },
-                }}
-              />
-            ))}
-          {selectedCategory === 'carpark' &&
-            carparkPositions.map((position) => (
-              <MapMarker
-                key={`carpark-${position.lat},${position.lng}`}
-                position={position}
-                image={{
-                  src: markerImageSrc,
-                  size: imageSize,
-                  options: {
-                    spriteSize: spriteSize,
-                    spriteOrigin: carparkOrigin,
-                  },
-                }}
-              />
-            ))}
-        </Map>
-        {/* 지도 위에 표시될 마커 카테고리 */}
-        <MapCategory setSelectedCategory={setSelectedCategory} />
+    <div className="map_wrap">
+      <div id="map" style={{ width: '100%', height: '100vh' }}>
+        <ul id="category">
+          <li id="BK9" data-order="0">
+            <span className="category_bg bank"></span>
+            은행
+          </li>
+          <li id="MT1" data-order="1">
+            <span className="category_bg mart"></span>
+            마트
+          </li>
+          <li id="PM9" data-order="2">
+            <span className="category_bg pharmacy"></span>
+            약국
+          </li>
+          <li id="OL7" data-order="3">
+            <span className="category_bg oil"></span>
+            주유소
+          </li>
+          <li id="CE7" data-order="4">
+            <span className="category_bg cafe"></span>
+            카페
+          </li>
+          <li id="CS2" data-order="5">
+            <span className="category_bg store"></span>
+            편의점
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
-}
+};
 
-export default KakaoMap;
+export default MapComponent;
