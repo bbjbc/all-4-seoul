83b4e268e51830239736fc02eec6c1c6b6709054
jae woo Jung <awfjol2008@naver.com>
2024-04-06 22:03:24 +0900

    refactor: User 엔티티 Setter 제거

--- backend/src/main/java/com/capstone/all4seoul/user/domain/User.java
+++ backend/src/main/java/com/capstone/all4seoul/user/domain/User.java
@@ -4,7 +4,6 @@ import com.capstone.all4seoul.place.domain.Place;
 import com.capstone.all4seoul.review.domain.Review;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,7 +11,6 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
