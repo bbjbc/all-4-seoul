ae82f966d3e6132db2e82a55edf0dc72137995c4
jae woo Jung <awfjol2008@naver.com>
2024-04-06 21:37:53 +0900

    refactor: Review 엔티티 setter 제거

--- backend/src/main/java/com/capstone/all4seoul/review/domain/Review.java
+++ backend/src/main/java/com/capstone/all4seoul/review/domain/Review.java
@@ -2,14 +2,21 @@ package com.capstone.all4seoul.review.domain;
 
 import com.capstone.all4seoul.place.domain.Place;
 import com.capstone.all4seoul.user.domain.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "reviews")
 public class Review {
     @Id
