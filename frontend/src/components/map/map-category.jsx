da0b5d1b1329f84b9746223de7a31204a16d4482
bbjbc <jobc4550@gmail.com>
2024-04-07 01:53:07 +0900

    feat: 카테고리 아이콘 변경

--- frontend/src/components/map/map-category.jsx
+++ frontend/src/components/map/map-category.jsx
@@ -4,36 +4,27 @@ import CategoryButton from '../button/category-button';
 
 import propTypes from 'prop-types';
 
-import { LuParkingCircle } from 'react-icons/lu';
-import { MdOutlineLocalCafe } from 'react-icons/md';
-import { RiRestaurantLine } from 'react-icons/ri';
-import { MdOutlineEventAvailable } from 'react-icons/md';
+import { FaSquareParking } from 'react-icons/fa6';
+import { MdLocalCafe } from 'react-icons/md';
+import { FaGasPump } from 'react-icons/fa6';
+import { MdOutlineRestaurant } from 'react-icons/md';
+import { MdOutlineAttractions } from 'react-icons/md';
 import { PiPersonArmsSpreadDuotone } from 'react-icons/pi';
 
 function MapCategory({ id }) {
   return (
     <div id={id} className="absolute left-16 top-24 z-20 flex flex-col gap-7">
-      <CategoryButton icon={LuParkingCircle} text="주차장" id="PK6" order="0" />
+      <CategoryButton icon={FaSquareParking} text="주차장" id="PK6" order="0" />
+      <CategoryButton icon={MdLocalCafe} text="카페" id="CE7" order="1" />
+      <CategoryButton icon={FaGasPump} text="주유소" id="OL7" order="2" />
       <CategoryButton
-        icon={MdOutlineLocalCafe}
-        text="카페"
-        id="CE7"
-        order="1"
-      />
-      <CategoryButton
-        icon={RiRestaurantLine}
-        text="주유소"
-        id="OL7"
-        order="2"
-      />
-      <CategoryButton
-        icon={MdOutlineEventAvailable}
+        icon={MdOutlineRestaurant}
         text="음식점"
         id="FD6"
         order="3"
       />
       <CategoryButton
-        icon={PiPersonArmsSpreadDuotone}
+        icon={MdOutlineAttractions}
         text="관광명소"
         id="AT4"
         order="4"
