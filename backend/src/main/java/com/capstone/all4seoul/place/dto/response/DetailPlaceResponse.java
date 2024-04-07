874901b79a8b65e5d98dce95c54080101d72cfa1
fbehddn <tom0611@naver.com>
2024-04-07 17:10:19 +0900

    refactor: 장소 조회 응답 DTO 메서드에서 리뷰 조회 응답 DTO를 받게 로직 수정

--- backend/src/main/java/com/capstone/all4seoul/place/dto/response/DetailPlaceResponse.java
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/response/DetailPlaceResponse.java
@@ -1,26 +1,33 @@
 package com.capstone.all4seoul.place.dto.response;
 
 import com.capstone.all4seoul.place.domain.Place;
-import com.capstone.all4seoul.review.domain.Review;
+import com.capstone.all4seoul.review.dto.response.DetailReviewResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DetailPlaceResponse {
     private String name;
-    private List<Review> reviews;
+    private List<DetailReviewResponse> reviews;
     private String phoneNumber;
 
     public static DetailPlaceResponse of(Place place) {
         DetailPlaceResponse detailPlaceResponse = new DetailPlaceResponse();
 
         detailPlaceResponse.name = place.getName();
-        detailPlaceResponse.reviews = place.getReviews();
+
+        // Place에 있는 Review 객체들을 DetailReviewResponse 객체로 변환하여 리스트에 담음
+        List<DetailReviewResponse> reviewResponses = place.getReviews().stream()
+                .map(review -> DetailReviewResponse.of(review))
+                .collect(Collectors.toList());
+        detailPlaceResponse.reviews = reviewResponses;
+
         detailPlaceResponse.phoneNumber = place.getPhoneNumber();
 
         return detailPlaceResponse;
