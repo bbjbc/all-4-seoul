bd3c79145d810ea3e6022e48639acda9dbc81554
fbehddn <tom0611@naver.com>
2024-04-07 16:58:53 +0900

    feat: 장소 전체조회 기능 구현

--- backend/src/main/java/com/capstone/all4seoul/place/service/PlaceService.java
+++ backend/src/main/java/com/capstone/all4seoul/place/service/PlaceService.java
@@ -1,4 +1,28 @@
 package com.capstone.all4seoul.place.service;
 
+import com.capstone.all4seoul.place.domain.Place;
+import com.capstone.all4seoul.place.dto.response.DetailPlaceResponse;
+import com.capstone.all4seoul.place.repository.PlaceRepository;
+import com.capstone.all4seoul.user.domain.User;
+import com.capstone.all4seoul.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PlaceService {
+    private final PlaceRepository placeRepository;
+    private final UserRepository userRepository;
+
+    //전체 장소 조회
+    public List<DetailPlaceResponse> findAll() {
+        return placeRepository.findAll()
+                .stream()
+                .map(DetailPlaceResponse::of)
+                .toList();
+    }
 }
