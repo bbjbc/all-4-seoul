2b3aefbca2b43ec8aa563bd01cfb6b4da829e244
fbehddn <tom0611@naver.com>
2024-04-07 17:11:33 +0900

    feat: PlaceController 클래스 생성

---
+++ backend/src/main/java/com/capstone/all4seoul/place/controller/PlaceController.java
@@ -0,0 +1,10 @@
+package com.capstone.all4seoul.place.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PlaceController {
+
+}
