924005ddda517da0ec53a41ced26ae301abf6c0b
fbehddn <tom0611@naver.com>
2024-04-07 17:01:30 +0900

    refactor: 장소 이름으로 조회 반환타입 리스트로 변경

--- backend/src/main/java/com/capstone/all4seoul/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/place/repository/PlaceRepository.java
@@ -3,15 +3,11 @@ package com.capstone.all4seoul.place.repository;
 import com.capstone.all4seoul.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-
     List<Place> findAll();
     List<Place> findListByUserId(Long userId);
-
-    Place findByName(String name);
-
+    List<Place> findByName(String name);
     void deleteById(Long placeId);
 }
