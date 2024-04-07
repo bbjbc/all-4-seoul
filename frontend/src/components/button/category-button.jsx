3131409d1d1901fb75a5a99ed2deff5d7b82a815
bbjbc <jobc4550@gmail.com>
2024-04-07 20:03:48 +0900

    feat: 카테고리 버튼 포커싱 해제하는 상태 추가

--- frontend/src/components/button/category-button.jsx
+++ frontend/src/components/button/category-button.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function CategoryButton({ icon: Icon, text, id, order }) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const focusHandler = () => {
+    setIsFocused(!isFocused);
+  };
+
+  const blurHandler = () => {
+    setIsFocused(false);
+  };
+
   return (
     <button
-      className="flex flex-col items-center space-y-2 rounded-lg border-2 border-gray-600 bg-blue-50 px-1 py-2 text-center shadow-xl hover:bg-cyan-300 focus:bg-yellow-100"
+      className={`flex flex-col items-center space-y-2 rounded-lg border-2 border-gray-600 bg-blue-50 px-1 py-2 text-center shadow-xl hover:bg-cyan-300 ${isFocused ? 'focus:bg-yellow-100' : ''}`}
       id={id}
       data-order={order}
+      onClick={focusHandler}
+      onBlur={blurHandler}
     >
       <Icon size={20} />
       <span>{text}</span>
