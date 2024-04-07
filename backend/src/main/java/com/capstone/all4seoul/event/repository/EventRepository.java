a3a040109fc760e4352af0e2e265f958f9e7da66
jae woo Jung <awfjol2008@naver.com>
2024-04-07 17:52:24 +0900

    feat: EventRepository 특정 기간에 해당하는 이벤트 목록 조회 메서드 구현

--- backend/src/main/java/com/capstone/all4seoul/event/repository/EventRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/event/repository/EventRepository.java
@@ -2,10 +2,17 @@ package com.capstone.all4seoul.event.repository;
 
 import com.capstone.all4seoul.event.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     Event findByName(String name);
+
+    @Query("SELECT e FROM Event e WHERE e.startDate >= :start AND e.endDate <= :end")
+    List<Event> findByPeriod(LocalDateTime startDate, LocalDateTime endDate);
 
     void deleteById(Long eventId);
 }
