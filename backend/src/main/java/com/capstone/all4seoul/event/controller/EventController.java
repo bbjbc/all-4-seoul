5cc29aa441a8c393ec29cc41117683dc23fcb110
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:54:09 +0900

    feat: EventController 생성

---
+++ backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.capstone.all4seoul.event.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+}
