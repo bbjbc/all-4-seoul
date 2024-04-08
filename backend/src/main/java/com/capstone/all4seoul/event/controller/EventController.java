5b7a296fbdba942d362fa7bfc967fc1c4a16c540
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:54:44 +0900

    feat: 이벤트 전체 조회 API 구현

--- backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
+++ backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
@@ -5,7 +5,11 @@ import com.capstone.all4seoul.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +23,13 @@ public class EventController {
     @GetMapping("/events/{eventId}")
     public DetailEventResponse listDetailEvent(@PathVariable Long eventId) {
         return eventService.findById(eventId);
+    }
+
+    /**
+     * 이벤트 전체 조회
+     */
+    @GetMapping("/events")
+    public List<DetailEventResponse> listAllEvents() {
+        return eventService.findAll();
     }
 }
