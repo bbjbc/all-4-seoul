73c2ce4e9c356c188ff03beb8fcadc179db39ff8
jae woo Jung <awfjol2008@naver.com>
2024-04-08 15:58:51 +0900

    fix: EventRepository의 기간에 따른 검색 메서드 파라미터에 @Param 키워드 추가

--- backend/src/main/java/com/capstone/all4seoul/event/repository/EventRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.capstone.all4seoul.event.repository;
 import com.capstone.all4seoul.event.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,7 +13,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Event findByName(String name);
 
     @Query("SELECT e FROM Event e WHERE e.startDate >= :start AND e.endDate <= :end")
-    List<Event> findByPeriod(LocalDateTime startDate, LocalDateTime endDate);
+    List<Event> findByPeriod(@Param("start") LocalDateTime startDate, @Param("end") LocalDateTime endDate);
 
     void deleteById(Long eventId);
 }
