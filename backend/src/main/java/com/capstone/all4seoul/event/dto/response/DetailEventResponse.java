feef366d0a504c766211a9247c913245dcea74cd
jae woo Jung <awfjol2008@naver.com>
2024-04-08 15:59:35 +0900

    fix: 이벤트 단건 조회 DTO 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/event/dto/response/DetailEventResponse.java
@@ -0,0 +1,29 @@
+package com.capstone.all4seoul.event.dto.response;
+
+import com.capstone.all4seoul.review.dto.response.DetailReviewResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailEventResponse {
+
+    private String name;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private int price;
+    private List<DetailReviewResponse> reviews;
+
+    // 문화행사가 일어나는 장소에 관한 데이터
+    private String placeName;
+    private String placePhoneNumber;
+    private String placeAddress;
+    private String placeX;
+    private String placeY;
+
+}
