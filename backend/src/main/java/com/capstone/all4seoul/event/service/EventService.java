d0efecc26970fc74d48117142902cee1121abbf7
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:40:13 +0900

    feat: 이벤트 단건 조회 기능 구현

--- backend/src/main/java/com/capstone/all4seoul/event/service/EventService.java
+++ backend/src/main/java/com/capstone/all4seoul/event/service/EventService.java
@@ -1,5 +1,7 @@
 package com.capstone.all4seoul.event.service;
 
+import com.capstone.all4seoul.event.dto.response.DetailEventResponse;
+import com.capstone.all4seoul.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class EventService {
+
+    private final EventRepository eventRepository;
+
+    /**
+     * 이벤트 단건 조회
+     */
+    public DetailEventResponse findById(Long eventId) {
+        return DetailEventResponse.of(eventRepository.findById(eventId).get());
+    }
 }
