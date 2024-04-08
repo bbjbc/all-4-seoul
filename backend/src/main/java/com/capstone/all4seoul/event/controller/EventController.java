3211d308bb677fbf1d62e1e4827b20d644ffcf7c
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:55:12 +0900

    feat: 특정 기간 내 이벤트 목록 조회 API 구현

--- backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
+++ backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
@@ -2,6 +2,7 @@ package com.capstone.all4seoul.event.controller;
 
 import com.capstone.all4seoul.event.dto.response.DetailEventResponse;
 import com.capstone.all4seoul.event.service.EventService;
+import com.capstone.all4seoul.user.dto.response.DetailUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,5 +32,16 @@ public class EventController {
     @GetMapping("/events")
     public List<DetailEventResponse> listAllEvents() {
         return eventService.findAll();
+    }
+
+    /**
+     * 특정 기간 내 이벤트 목록 조회
+     */
+    @GetMapping("/events/period")
+    public List<DetailEventResponse> listEventsByPeriod(
+            @RequestParam LocalDateTime startDateTime,
+            @RequestParam LocalDateTime endDateTime
+    ) {
+        return eventService.findEventsByPeriod(startDateTime, endDateTime);
     }
 }
