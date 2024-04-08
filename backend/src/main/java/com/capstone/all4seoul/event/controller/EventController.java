f2d794b2b9c9d621413e97d212a7fd6911ed1d09
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:55:42 +0900

    feat: 특정 좌표 기준 반경 내 이벤트 목록 조회 API 구현

--- backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
+++ backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
@@ -2,7 +2,6 @@ package com.capstone.all4seoul.event.controller;
 
 import com.capstone.all4seoul.event.dto.response.DetailEventResponse;
 import com.capstone.all4seoul.event.service.EventService;
-import com.capstone.all4seoul.user.dto.response.DetailUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,5 +42,17 @@ public class EventController {
             @RequestParam LocalDateTime endDateTime
     ) {
         return eventService.findEventsByPeriod(startDateTime, endDateTime);
+    }
+
+    /**
+     * 특정 좌표 기준 반경 내 이벤트 목록 조회
+     */
+    @GetMapping("/events/location")
+    public List<DetailEventResponse> listEventsByLocation(
+            @RequestParam Double x,
+            @RequestParam Double y,
+            @RequestParam int radius
+    ) {
+        return eventService.findEventsByLocation(x, y, radius);
     }
 }
