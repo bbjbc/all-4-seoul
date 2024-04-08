eb67e398fee657bf2f59f840e7c172f1010ae7c1
jae woo Jung <awfjol2008@naver.com>
2024-04-08 21:54:26 +0900

    feat: 이벤트 단건 조회 API 구현

--- backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
+++ backend/src/main/java/com/capstone/all4seoul/event/controller/EventController.java
@@ -1,9 +1,23 @@
 package com.capstone.all4seoul.event.controller;
 
+import com.capstone.all4seoul.event.dto.response.DetailEventResponse;
+import com.capstone.all4seoul.event.service.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class EventController {
+
+    private final EventService eventService;
+
+    /**
+     * 이벤트 단건 조회
+     */
+    @GetMapping("/events/{eventId}")
+    public DetailEventResponse listDetailEvent(@PathVariable Long eventId) {
+        return eventService.findById(eventId);
+    }
 }
