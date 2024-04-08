adb735ea811b6411f7da4d48475172394cc0cbc2
jae woo Jung <awfjol2008@naver.com>
2024-04-08 15:56:51 +0900

    feat: insert.sql 엔티티 필드 변경사항 적용 및 events 테이블 더미 데이터 추가

--- backend/src/main/resources/insert.sql
+++ backend/src/main/resources/insert.sql
@@ -1,3 +1,4 @@
+-- users 테이블에 더미 데이터 추가
 insert into users (user_id, login_id, login_password, name, birth, mbti, gender, nickname)
 values (1, 'tdawkes0@is.gd', 'zI4`(Q,udf40}WJ', 'Avigdor Meigh', '2023-08-16 22:21:22', 'ISTP', 'Male',
         'Tabor Dawkes');
@@ -5,11 +6,19 @@ insert into users (user_id, login_id, login_password, name, birth, mbti, gender,
 values (2, 'lbittany1@ftc.gov', 'jS1#`89eGg8%OesC', 'Thaddus McCullouch', '2023-10-30 05:45:08', 'ISFP',
         'Male', 'Laurie Bittany');
 
--- Places 테이블에 더미 데이터 추가
-INSERT INTO places (place_id, user_id, phone_number, x, y, DTYPE, name)
-VALUES (1, 1, '01012345678', '127.012', '37.567', 'place', 'Place One'),
-       (2, 2, '01087654321', '128.123', '38.678', 'place', 'Place Two');
+-- places 테이블에 더미 데이터 추가
+INSERT INTO places (place_id, user_id, phone_number, address, x, y, DTYPE, name)
+VALUES (1, 1, '01012345678', '광교산로 뭐시기', '127.012', '37.567', 'place', 'Place One'),
+       (2, 2, '01087654321', '보정로 30', '128.123', '38.678', 'place', 'Place Two');
 
+-- events 테이블에 더미 데이터 추가
+INSERT INTO events (event_id, name, start_date, end_date, price, place_id, x, y)
+VALUES (1, '테스트 이벤트1', '2024-04-20 10:00:00', '2024-04-22 20:00:00', 50000, 1, '127.002', '37.002');
+
+INSERT INTO events (event_id, name, start_date, end_date, price, place_id, x, y)
+VALUES (2, '테스트 이벤트2', '2024-05-01 09:00:00', '2024-05-03 18:00:00', 30000, 1, '127.003', '37.003');
+
+-- reviews 테이블에 더미 데이터 추가
 insert into reviews (review_id, user_id, star_rating, content, place_id)
 values (1, 1, 1.4,
         'Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 1);
