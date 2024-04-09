4b17b2ea07d8b6b949b708af9bb0347753946e03
jae woo Jung <awfjol2008@naver.com>
2024-04-09 14:06:55 +0900

    feat: ReviewRepository 장소에 따른 리뷰 목록 조회 메서드 구현

--- backend/src/main/java/com/capstone/all4seoul/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/review/repository/ReviewRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    //유저 아이디로 리뷰 리스트 조회
     List<Review> findByUserLoginId(String loginId);
 
     //장소 이름으로 리뷰 리스트 조회
     List<Review> findByPlaceName(String placeId);
+    List<Review> findByPlaceId(Long placeId);
 }
