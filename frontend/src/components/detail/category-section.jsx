c9408595b7fdd263db8a37fbe9e94965499df7e2
bbjbc <jobc4550@gmail.com>
2024-04-13 17:02:09 +0900

    feat: 카테고리 클릭 시 스크롤되도록 ref 추가 및 스타일링 수정

--- frontend/src/components/detail/category-section.jsx
+++ frontend/src/components/detail/category-section.jsx
@@ -1,56 +1,67 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
-
 import propTypes from 'prop-types';
 
-import { BsArrowLeftCircle } from 'react-icons/bs';
 import { GiMatterStates } from 'react-icons/gi';
 import { PiPersonArmsSpreadDuotone } from 'react-icons/pi';
 import { MdOutlineAttractions } from 'react-icons/md';
-import { GiGasPump } from 'react-icons/gi';
+import { TiWeatherPartlySunny } from 'react-icons/ti';
+import { RiParkingLine } from 'react-icons/ri';
 import { GoCodeReview } from 'react-icons/go';
 
-function CategorySection({ activeCategory, handleCategoryClick }) {
-  const navigation = useNavigate();
-
-  const handleBack = () => {
-    navigation('/list');
-  };
-
+function CategorySection({ activeCategory, handleCategoryClick, refs }) {
   const categories = [
-    { name: '현재 상태', icon: <GiMatterStates size={20} /> },
-    { name: '인구 정보', icon: <PiPersonArmsSpreadDuotone size={20} /> },
-    { name: '문화행사', icon: <MdOutlineAttractions size={20} /> },
-    { name: '주차장', icon: <GiGasPump size={20} /> },
-    { name: '리뷰 작성', icon: <GoCodeReview size={20} /> },
+    {
+      name: '현재 상태',
+      icon: <GiMatterStates size={20} />,
+      ref: refs['현재 상태'],
+    },
+    {
+      name: '인구 정보',
+      icon: <PiPersonArmsSpreadDuotone size={20} />,
+      ref: refs['인구 정보'],
+    },
+    {
+      name: '문화행사',
+      icon: <MdOutlineAttractions size={20} />,
+      ref: refs['문화 행사'],
+    },
+    {
+      name: '날씨 정보',
+      icon: <TiWeatherPartlySunny size={20} />,
+      ref: refs['날씨 정보'],
+    },
+    {
+      name: '주차장',
+      icon: <RiParkingLine size={20} />,
+      ref: refs['주차장 정보'],
+    },
+    {
+      name: '리뷰 작성',
+      icon: <GoCodeReview size={20} />,
+      ref: refs['리뷰 작성'],
+    },
   ];
 
   return (
     <>
-      <div className="absolute left-44 top-32">
-        <button
-          className="text-2xl text-zinc-900 hover:text-neutral-500"
-          onClick={handleBack}
-        >
-          <BsArrowLeftCircle size={30} />
-        </button>
-      </div>
-      <div className="absolute left-60 flex flex-col justify-center space-y-9">
-        {categories.map((cat) => (
-          <button
-            key={cat.name}
-            className={`flex flex-col items-center rounded-2xl px-4 py-3 shadow-md ${
-              activeCategory === cat.name
-                ? 'bg-amber-100 hover:bg-amber-200'
-                : 'bg-gray-200 hover:bg-gray-100'
-            }`}
-            onClick={() => handleCategoryClick(cat.name)}
-          >
-            {cat.icon}
-            <span className="mt-2">{cat.name}</span>
-          </button>
-        ))}
+      <div className="fixed left-0 top-12 z-50 h-full overflow-y-auto bg-white bg-opacity-90 p-1">
+        <div className="flex flex-col space-y-10">
+          {categories.map((cat) => (
+            <button
+              key={cat.name}
+              className={`flex flex-col items-center rounded-lg px-4 py-2 ${
+                activeCategory === cat.name
+                  ? 'bg-amber-100 hover:bg-amber-200'
+                  : 'hover:bg-slate-200'
+              }`}
+              onClick={() => handleCategoryClick(cat.name, cat.ref)}
+            >
+              {cat.icon}
+              <span className="mt-2">{cat.name}</span>
+            </button>
+          ))}
+        </div>
       </div>
     </>
   );
@@ -59,6 +70,7 @@ function CategorySection({ activeCategory, handleCategoryClick }) {
 CategorySection.propTypes = {
   activeCategory: propTypes.string.isRequired,
   handleCategoryClick: propTypes.func.isRequired,
+  refs: propTypes.object.isRequired,
 };
 
 export default CategorySection;
