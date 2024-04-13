cd21f94135722cc9c28fb3d64a78a3181f97bc9e
bbjbc <jobc4550@gmail.com>
2024-04-13 17:02:35 +0900

    feat: 스크롤링을 위한 ref 추가 및 뒤로가기 버튼 추가

--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -1,75 +1,82 @@
 import React, { useState, useRef } from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import RealTimePopulation from '../components/detail/real-time-population';
 import CategorySection from '../components/detail/category-section';
 import DetailItem from '../components/detail/detail-item';
+import PopulationInfo from '../components/detail/population-info';
+import WeatherInfo from '../components/detail/weather-info';
+import CultureEvent from '../components/detail/culture-event';
+import ParkingInfo from '../components/detail/parking-info';
+import Review from '../components/detail/review';
+import { IoIosArrowBack } from 'react-icons/io';
 
 function PlaceDetailPage() {
+  const navigation = useNavigate();
   const [activeCategory, setActiveCategory] = useState(null);
 
   const { id } = useParams();
   const decodedName = decodeURIComponent(id);
-  const scrollRef = useRef(null);
 
-  const handleCategoryClick = (category) => {
+  const realTimeRef = useRef(null);
+  const populationRef = useRef(null);
+  const weatherRef = useRef(null);
+  const cultureRef = useRef(null);
+  const parkingRef = useRef(null);
+  const reviewRef = useRef(null);
+
+  const handleCategoryClick = (category, ref) => {
     setActiveCategory(category);
-    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+    console.log(ref.current);
+  };
+
+  const handleBack = () => {
+    navigation('/list');
   };
 
   return (
     <>
       <div className="relative flex animate-fadein items-center justify-center px-4 py-36">
+        <div className="absolute left-40 top-28">
+          <button
+            className="text-md flex flex-row text-zinc-900 hover:text-neutral-500"
+            onClick={handleBack}
+          >
+            <IoIosArrowBack size={20} />
+            뒤로가기
+          </button>
+        </div>
         <CategorySection
           activeCategory={activeCategory}
           handleCategoryClick={handleCategoryClick}
+          refs={{
+            '현재 상태': realTimeRef,
+            '인구 정보': populationRef,
+            '문화 행사': cultureRef,
+            '날씨 정보': weatherRef,
+            '주차장 정보': parkingRef,
+            '리뷰 작성': reviewRef,
+          }}
         />
         <DetailItem decodedName={decodedName} />
       </div>
 
       <div className="flex items-center justify-center">
-        <div
-          className="mx-10 flex w-3/5 flex-col items-center justify-center space-y-10"
-          ref={scrollRef}
-        >
-          {/* 현재 상태 */}
-          <RealTimePopulation name={decodedName} congestionLevel="보통" />
-
-          {/* 인구정보 */}
-          <div className="rounded-xl bg-gray-100 p-4">
-            <div className="mb-4">
-              <h3 className="text-md mb-2 font-semibold">실시간 인구정보</h3>
-            </div>
-            <div>
-              <h3 className="text-md mb-2 font-semibold">날씨 정보</h3>
-            </div>
-          </div>
-          {/* 문화행사 */}
-          <div className="rounded-xl bg-gray-100 p-4">
-            <h2 className="mb-2 text-lg font-semibold">문화행사</h2>
-          </div>
-          {/* 주차장 */}
-          <div className="rounded-xl bg-gray-100 p-4">
-            <h2 className="mb-2 text-lg font-semibold">주차장 정보</h2>
-            <p>주차 가능한 공간이 있습니다.</p>
-          </div>
-          {/* 리뷰 작성 */}
-          <div className="rounded-xl bg-gray-100 p-4">
-            <h2 className="mb-2 text-lg font-semibold">리뷰 작성</h2>
-            <form>
-              <textarea
-                className="h-32 w-full rounded-md border border-gray-300 p-2"
-                placeholder="리뷰를 작성해주세요."
-              ></textarea>
-              <button
-                type="submit"
-                className="mt-2 rounded-md bg-blue-500 px-4 py-2 text-white transition-colors duration-300 ease-in-out hover:bg-blue-700"
-              >
-                작성하기
-              </button>
-            </form>
-          </div>
+        <div className="mx-10 flex w-3/5 flex-col items-center justify-center space-y-10">
+          <RealTimePopulation
+            name={decodedName}
+            congestionLevel="보통"
+            realtimeRef={realTimeRef}
+          />
+          <PopulationInfo populationRef={populationRef} />
+          <WeatherInfo weatherRef={weatherRef} />
+          <CultureEvent cultureRef={cultureRef} />
+          <ParkingInfo parkingRef={parkingRef} />
+          <Review reviewRef={reviewRef} />
         </div>
       </div>
     </>
