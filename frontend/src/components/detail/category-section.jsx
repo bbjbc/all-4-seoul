e67e0ed55f7d082a25189035f46a4c073fbd4e0f
bbjbc <jobc4550@gmail.com>
2024-04-13 21:25:37 +0900

    style: 상세페이지 카테고리 섹션 스타일 수정

--- frontend/src/components/detail/category-section.jsx
+++ frontend/src/components/detail/category-section.jsx
@@ -13,62 +13,60 @@ function CategorySection({ activeCategory, handleCategoryClick, refs }) {
   const categories = [
     {
       name: '현재 상태',
-      icon: <GiMatterStates size={20} />,
+      icon: <GiMatterStates size={30} />,
       ref: refs['현재 상태'],
     },
     {
       name: '인구 정보',
-      icon: <PiPersonArmsSpreadDuotone size={20} />,
+      icon: <PiPersonArmsSpreadDuotone size={30} />,
       ref: refs['인구 정보'],
     },
     {
       name: '문화행사',
-      icon: <MdOutlineAttractions size={20} />,
+      icon: <MdOutlineAttractions size={30} />,
       ref: refs['문화 행사'],
     },
     {
       name: '날씨 정보',
-      icon: <TiWeatherPartlySunny size={20} />,
+      icon: <TiWeatherPartlySunny size={30} />,
       ref: refs['날씨 정보'],
     },
     {
       name: '주차장',
-      icon: <RiParkingLine size={20} />,
+      icon: <RiParkingLine size={30} />,
       ref: refs['주차장 정보'],
     },
     {
       name: '리뷰 작성',
-      icon: <GoCodeReview size={20} />,
+      icon: <GoCodeReview size={30} />,
       ref: refs['리뷰 작성'],
     },
   ];
 
   return (
-    <>
-      <div className="fixed left-0 top-12 z-50 h-full overflow-y-auto bg-white bg-opacity-90 p-1">
-        <div className="flex flex-col space-y-10">
-          {categories.map((cat) => (
-            <button
-              key={cat.name}
-              className={`flex flex-col items-center rounded-lg px-4 py-2 ${
-                activeCategory === cat.name
-                  ? 'bg-amber-100 hover:bg-amber-200'
-                  : 'hover:bg-slate-200'
-              }`}
-              onClick={() => handleCategoryClick(cat.name, cat.ref)}
-            >
-              {cat.icon}
-              <span className="mt-2">{cat.name}</span>
-            </button>
-          ))}
-        </div>
+    <div className="fixed left-0 z-50 h-full overflow-y-auto bg-white p-1 pt-16">
+      <div className="flex flex-col space-y-7">
+        {categories.map((cat) => (
+          <button
+            key={cat.name}
+            className={`flex flex-col items-center rounded-lg px-3 py-2 ${
+              activeCategory === cat.name
+                ? 'bg-amber-100 transition-all duration-200 ease-in-out hover:bg-amber-200'
+                : 'transition-all duration-200 ease-in-out hover:bg-slate-300'
+            }`}
+            onClick={() => handleCategoryClick(cat.name, cat.ref)}
+          >
+            {cat.icon}
+            <span className="mt-2">{cat.name}</span>
+          </button>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
 CategorySection.propTypes = {
-  activeCategory: propTypes.string.isRequired,
+  activeCategory: propTypes.string,
   handleCategoryClick: propTypes.func.isRequired,
   refs: propTypes.object.isRequired,
 };
