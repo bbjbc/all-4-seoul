c6636be0cdf1515af2f247551cc799d7e746171f
fbehddn <tom0611@naver.com>
2024-04-18 16:21:00 +0900

    feat: 구글 API 응답형식에 따른 PlaceSearchResponse 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/response/PlaceSearchResponse.java
@@ -0,0 +1,116 @@
+package com.capstone.all4seoul.place.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.net.URI;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceSearchResponse {
+
+    private List<Result> results;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Result {
+        private DisplayName displayName;
+        private String placeId;
+        private CurrentOpeningHours currentOpeningHours;
+        private String nationalPhoneNumber;
+        private Reviews reviews;
+        //총 별점 평균
+        private Double rating;
+        private URI websiteUri;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class DisplayName {
+        String text;
+        String languageCode;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class CurrentOpeningHours {
+        Boolean openNow;
+        private Periods periods;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Reviews {
+        //리뷰 고유 url
+        private String name;
+        //몇달전
+        private String relativePublishTimeDescription;
+        //별점
+        private Double rating;
+        private Text text;
+        private Text originalText;
+        private AuthorAttribution authorAttribution;
+        private String publishTime;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Text {
+        private String text;
+        private String languageCode;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class AuthorAttribution {
+        private String displayName;
+        private String uri;
+        private String photoUri;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Periods{
+        private Open open;
+        private Close close;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Open{
+        private int day;
+        private int hour;
+        private int minute;
+        private Date date;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Close{
+        private int day;
+        private int hour;
+        private int minute;
+        private Date date;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Date {
+        String year;
+        String month;
+        String day;
+    }
+}
