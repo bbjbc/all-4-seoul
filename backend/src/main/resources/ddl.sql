7ad8120f5ab9b8e9c77a123f1230c090aa9e7eff
jae woo Jung <awfjol2008@naver.com>
2024-04-19 15:00:58 +0900

    feat: 각 엔티티 변경에 따른 ddl.sql 변경

--- backend/src/main/resources/ddl.sql
+++ backend/src/main/resources/ddl.sql
@@ -1,15 +1,17 @@
--- 테이블 삭제 전 외래 키 제약 조건 삭제
-ALTER TABLE places DROP FOREIGN KEY FK_places_users;
-ALTER TABLE events DROP FOREIGN KEY FK_events_places;
-ALTER TABLE parking_lot DROP FOREIGN KEY FK_parking_lot_places;
-ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_users;
-ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_places;
-ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_events;
+# -- 테이블 삭제 전 외래 키 제약 조건 삭제
+# ALTER TABLE bookmarks DROP FOREIGN KEY FK_bookmarks_users;
+# ALTER TABLE bookmarks DROP FOREIGN KEY FK_bookmarks_places;
+# ALTER TABLE events DROP FOREIGN KEY FK_events_places;
+# ALTER TABLE parking_lot DROP FOREIGN KEY FK_parking_lot_places;
+# ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_users;
+# ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_places;
+# ALTER TABLE reviews DROP FOREIGN KEY FK_reviews_events;
 
 -- 테이블 삭제
 DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS parking_lot;
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS bookmarks;
 DROP TABLE IF EXISTS places;
 DROP TABLE IF EXISTS users;
 
@@ -18,29 +20,34 @@ CREATE TABLE users (
                        user_id BIGINT NOT NULL AUTO_INCREMENT,
                        birth DATE NOT NULL,
                        credit INTEGER DEFAULT 0,
-                       login_id VARCHAR(30) NOT NULL,
+                       login_id VARCHAR(30) NOT NULL UNIQUE,
                        name VARCHAR(30) NOT NULL,
-                       nickname VARCHAR(30) NOT NULL,
+                       nickname VARCHAR(30) NOT NULL UNIQUE,
                        login_password VARCHAR(100) NOT NULL,
                        gender ENUM('Male', 'Female') NOT NULL,
                        mbti ENUM('INTJ', 'INTP', 'INFJ', 'INFP', 'ISTJ', 'ISFJ', 'ISTP', 'ISFP', 'ENTJ', 'ENTP', 'ENFJ', 'ENFP', 'ESTJ', 'ESFJ', 'ESTP', 'ESFP') NOT NULL,
-                       PRIMARY KEY (user_id),
-                       UNIQUE KEY UK_i3xs7wmfu2i3jt079uuetycit (login_id),
-                       UNIQUE KEY UK_2ty1xmrrgtn89xt7kyxx6ta7h (nickname)
+                       PRIMARY KEY (user_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE places (
                         place_id BIGINT NOT NULL AUTO_INCREMENT,
-                        user_id BIGINT,
-                        phone_number VARCHAR(11) NOT NULL,
+                        phone_number VARCHAR(11) NOT NULL UNIQUE,
                         address VARCHAR(100) NOT NULL,
                         x DOUBLE NOT NULL,
                         y DOUBLE NOT NULL,
                         DTYPE VARCHAR(31) NOT NULL,
                         name VARCHAR(100) NOT NULL,
-                        PRIMARY KEY (place_id),
-                        UNIQUE KEY UK_ax1t8ehbn4dqi0avgjsu0n86q (phone_number),
-                        CONSTRAINT FK_places_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+                        PRIMARY KEY (place_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE bookmarks (
+                           bookmark_id BIGINT NOT NULL AUTO_INCREMENT,
+                           user_id BIGINT NOT NULL,
+                           place_id BIGINT NOT NULL,
+                           PRIMARY KEY (bookmark_id),
+                           CONSTRAINT FK_bookmarks_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+                           CONSTRAINT FK_bookmarks_places FOREIGN KEY (place_id) REFERENCES places(place_id) ON DELETE CASCADE,
+                           UNIQUE (user_id, place_id)  -- 이 구문은 한 사용자가 같은 장소에 중복 북마크를 허용하지 않습니다.
 ) ENGINE=InnoDB;
 
 -- 이어서 events, parking_lot, reviews 테이블 생성
