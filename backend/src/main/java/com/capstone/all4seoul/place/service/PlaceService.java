ed933474085e4e7b72d5c49f7fc097fef5d7a42b
fbehddn <tom0611@naver.com>
2024-04-19 14:38:13 +0900

    feat: PlaceService 클래스 유저 관련 로직 제거

--- backend/src/main/java/com/capstone/all4seoul/place/service/PlaceService.java
+++ backend/src/main/java/com/capstone/all4seoul/place/service/PlaceService.java
@@ -1,9 +1,7 @@
 package com.capstone.all4seoul.place.service;
 
-import com.capstone.all4seoul.place.domain.Place;
 import com.capstone.all4seoul.place.dto.response.DetailPlaceResponse;
 import com.capstone.all4seoul.place.repository.PlaceRepository;
-import com.capstone.all4seoul.user.domain.User;
 import com.capstone.all4seoul.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,26 +28,12 @@ public class PlaceService {
                 .map(DetailPlaceResponse::of)
                 .toList();
     }
-    //유저 한 명에 대해 북마크 조회
-    public List<DetailPlaceResponse> findPlacesByUserId(Long userId) {
-        return placeRepository.findListByUserId(userId)
-                .stream()
-                .map(DetailPlaceResponse::of)
-                .toList();
-    }
+
     //장소 이름으로 장소 리스트 조회
     public List<DetailPlaceResponse> findPlacesByName(String name) {
         return placeRepository.findByName(name)
                 .stream()
                 .map(DetailPlaceResponse::of)
                 .toList();
-    }
-    // 유저의 북마크에 장소 추가
-    @Transactional
-    public void addPlaceToUserBookmark(Long userId, Long placeId) {
-        User user = userRepository.findById(userId).get();
-        Place place = placeRepository.findById(placeId).get();
-
-        user.getPlaces().add(place);
     }
 }
