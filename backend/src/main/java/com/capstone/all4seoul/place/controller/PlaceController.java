4af5cd8e869afcb9e827386478f7c001d577a691
fbehddn <tom0611@naver.com>
2024-04-19 14:31:59 +0900

    feat: Place 컨트롤러 유저관련 로직 제거

--- backend/src/main/java/com/capstone/all4seoul/place/controller/PlaceController.java
+++ backend/src/main/java/com/capstone/all4seoul/place/controller/PlaceController.java
@@ -1,12 +1,13 @@
 package com.capstone.all4seoul.place.controller;
 
+import com.capstone.all4seoul.place.dto.request.PlaceSearchRequest;
 import com.capstone.all4seoul.place.dto.response.DetailPlaceResponse;
+import com.capstone.all4seoul.place.dto.response.PlaceSearchResponse;
 import com.capstone.all4seoul.place.service.PlaceService;
+import com.capstone.all4seoul.place.service.PlaceServiceByGoogle;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,21 +22,15 @@ public class PlaceController {
         return placeService.findAll();
     }
 
-    //유저 한 명에 대해 북마크 조회
-    @GetMapping("/users/{userId}/places")
-    public List<DetailPlaceResponse> getPlacesByUserId(@PathVariable Long userId) {
-        return placeService.findPlacesByUserId(userId);
-    }
-
     //장소 이름으로 장소 리스트 조회
     @GetMapping("/places/{name}")
     public List<DetailPlaceResponse> getPlacesByName(@PathVariable String name) {
         return placeService.findPlacesByName(name);
     }
 
-    //유저 북마크에 장소 추가
-    @PostMapping("/users/{userId}/places/{placeId}")
-    public void addPlaceToUserBookmark(@PathVariable Long userId, @PathVariable Long placeId) {
-        placeService.addPlaceToUserBookmark(userId, placeId);
+    //구글 api 장소 검색
+    @PostMapping("/places/google")
+    public PlaceSearchResponse searchPlacesByGoogle(@RequestBody PlaceSearchRequest placeSearchRequest) {
+        return PlaceServiceByGoogle.searchPlaces(placeSearchRequest);
     }
 }
