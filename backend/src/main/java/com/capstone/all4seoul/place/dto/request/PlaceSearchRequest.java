7416ac560ddf3c85ad0cd880150ad45ee6c4e24a
fbehddn <tom0611@naver.com>
2024-04-19 14:34:52 +0900

    refactor: 구글 API 요청 DTO @Setter 추가

--- backend/src/main/java/com/capstone/all4seoul/place/dto/request/PlaceSearchRequest.java
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/request/PlaceSearchRequest.java
@@ -3,10 +3,12 @@ package com.capstone.all4seoul.place.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlaceSearchRequest {
@@ -15,25 +17,29 @@ public class PlaceSearchRequest {
     private LocationRestriction locationRestriction;
 }
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-class LocationRestriction {
-    private Circle circle;
-}
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LocationRestriction {
+        private Circle circle;
+    }
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-class Circle {
-    private Center center;
-    private double radius;
-}
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Circle {
+        private Center center;
+        private double radius;
+    }
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-class Center {
-    private double latitude;
-    private double longitude;
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Center {
+        private double latitude;
+        private double longitude;
+    }
 }
