df816e3f12b7393bcb8df924faef5098f7ada323
jae woo Jung <awfjol2008@naver.com>
2024-04-21 21:18:46 +0900

    feat: 장소 상세조회 DTO에 북마크 개수 필드 추가

--- backend/src/main/java/com/capstone/all4seoul/place/dto/response/DetailPlaceResponse.java
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/response/DetailPlaceResponse.java
@@ -3,31 +3,28 @@ package com.capstone.all4seoul.place.dto.response;
 import com.capstone.all4seoul.place.domain.Place;
 import com.capstone.all4seoul.review.dto.response.DetailReviewResponse;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DetailPlaceResponse {
     private String name;
-    private List<DetailReviewResponse> reviews;
     private String phoneNumber;
+    private int bookmarkCount;
+    private List<DetailReviewResponse> reviews;
 
     public static DetailPlaceResponse of(Place place) {
         DetailPlaceResponse detailPlaceResponse = new DetailPlaceResponse();
 
         detailPlaceResponse.name = place.getName();
-
-        // Place에 있는 Review 객체들을 DetailReviewResponse 객체로 변환하여 리스트에 담음
+        detailPlaceResponse.phoneNumber = place.getPhoneNumber();
+        detailPlaceResponse.bookmarkCount = place.getBookmarks().size();
         detailPlaceResponse.reviews = place.getReviews().stream()
                 .map(DetailReviewResponse::of)
                 .toList();
-
-        detailPlaceResponse.phoneNumber = place.getPhoneNumber();
 
         return detailPlaceResponse;
     }
