b63642a45de5dc5369abf072bf647c5571fbd2c3
jae woo Jung <awfjol2008@naver.com>
2024-04-22 10:51:14 +0900

    refactor: UserRepository Optional 적용

--- backend/src/main/java/com/capstone/all4seoul/user/repository/UserRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/user/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.capstone.all4seoul.user.repository;
 
 import com.capstone.all4seoul.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     /**
@@ -13,11 +13,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
      */
 
     //로그인 아이디로 조회
-    User findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
+
+    //단건 조회
+    Optional<User> findFirstByUsername(String username);
 
     //컬렉션 조회
     List<User> findListByUsername(String username);
-
-    //단건 조회
-    User findFirstByUsername(String username);
 }
