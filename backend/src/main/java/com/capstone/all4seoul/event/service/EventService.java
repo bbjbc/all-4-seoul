60bb5f12053b678406774afe2fd70d96d2df08bc
jae woo Jung <awfjol2008@naver.com>
2024-04-22 11:38:57 +0900

    refactor: EventService Optional 적용

--- backend/src/main/java/com/capstone/all4seoul/event/service/EventService.java
+++ backend/src/main/java/com/capstone/all4seoul/event/service/EventService.java
@@ -2,6 +2,7 @@ package com.capstone.all4seoul.event.service;
 
 import com.capstone.all4seoul.event.dto.response.DetailEventResponse;
 import com.capstone.all4seoul.event.repository.EventRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +21,10 @@ public class EventService {
      * 이벤트 단건 조회
      */
     public DetailEventResponse findById(Long eventId) {
-        return DetailEventResponse.of(eventRepository.findById(eventId).get());
+        return DetailEventResponse.of(
+                eventRepository.findById(eventId)
+                        .orElseThrow(() -> new EntityNotFoundException("이벤트를 찾을 수 없습니다."))
+        );
     }
 
     /**
