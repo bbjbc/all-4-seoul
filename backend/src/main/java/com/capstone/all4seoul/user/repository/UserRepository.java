318aaa344a8527f8dd66172fdf1caa263ddb5717
jae woo Jung <awfjol2008@naver.com>
2024-04-22 12:20:00 +0900

    refactor: 사용자 도메인 주석 제거

--- backend/src/main/java/com/capstone/all4seoul/user/repository/UserRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/user/repository/UserRepository.java
@@ -7,17 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    /**
-     * Spring Data JPA
-     * save(), delete() 는 자동구현 되어있음
-     */
-
-    //로그인 아이디로 조회
     Optional<User> findByLoginId(String loginId);
-
-    //단건 조회
     Optional<User> findFirstByUsername(String username);
-
-    //컬렉션 조회
     List<User> findListByUsername(String username);
 }
