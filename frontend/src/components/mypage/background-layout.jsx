54400217a6edfa1a6cf0ac0d9759a3b97b178b93
bbjbc <jobc4550@gmail.com>
2024-04-26 19:27:47 +0900

    feat: 경로가 mypage일 경우 뒤로 가기 버튼 렌더링 검증

--- frontend/src/components/mypage/background-layout.jsx
+++ frontend/src/components/mypage/background-layout.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 import propTypes from 'prop-types';
 
 import logo from '../../assets/올포서울로고.jpg';
 import HandleBackButton from '../button/handle-back-button';
 
 function BackgroundLayout({ children }) {
+  const location = useLocation();
+
+  const isMypage = location.pathname === '/mypage';
+
   return (
     <>
-      <HandleBackButton path="mypage" />
-      <div className="relative flex h-screen items-center justify-center overflow-hidden">
+      {!isMypage && <HandleBackButton path="mypage" />}
+      <div className="relative flex h-screen justify-center overflow-hidden">
         <img
           className="absolute inset-0 z-0 h-full w-full object-cover opacity-10"
           src={logo}
