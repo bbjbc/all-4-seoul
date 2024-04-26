9f2a5ef9c368b37de0a5fcf729337aca6e83525b
bbjbc <jobc4550@gmail.com>
2024-04-26 23:33:47 +0900

    style: 북마크 페이지 스타일 수정

--- frontend/src/components/mypage/bookmarked.jsx
+++ frontend/src/components/mypage/bookmarked.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import { useBookmark } from '../../state/bookmark-context';
 
-function Bookmarked() {
+function BookmarkedPage() {
   const { bookmarks } = useBookmark();
 
   return (
     <div className="flex items-center justify-center">
-      <div className="relative z-10 flex w-2/3 flex-col items-start justify-center">
-        <p className="my-5 text-xl font-extrabold">
-          북마크 개수({bookmarks.length})
-        </p>
-        <div className="flex h-[500px] w-full flex-wrap justify-center gap-4 overflow-y-auto rounded-lg bg-white px-2 py-6 shadow-lg">
+      <div className="relative z-10 flex w-full flex-col items-start justify-center">
+        <div className="my-10 flex h-[500px] w-full flex-wrap justify-center gap-4 overflow-y-auto rounded-lg bg-white px-2 py-6 shadow-lg">
+          <p className="text-xl font-extrabold">
+            북마크 개수 ({bookmarks.length}개)
+          </p>
           {bookmarks.length === 0 ? (
-            <div className="flex w-full flex-1 flex-col items-center justify-center">
+            <div className="flex w-full flex-col">
               <p className="animate-bounce text-lg">북마크 목록이 없습니다.</p>
             </div>
           ) : (
-            bookmarks.map((bookmark) => (
-              <article key={bookmark.id} className="max-w-xs">
-                <div className="rounded-xl bg-white text-center shadow-xl">
-                  <img
-                    src={bookmark.images}
-                    alt={bookmark.name}
-                    className="h-44 w-44 rounded-t-xl object-cover"
-                  />
-                  <div className="p-2 text-center">
-                    <h3 className="text-sm font-semibold">{bookmark.name}</h3>
-                  </div>
-                </div>
-              </article>
-            ))
+            <div className="w-full px-12">
+              <div className="grid grid-cols-5 gap-4">
+                {bookmarks.map((bookmark) => (
+                  <article
+                    key={bookmark.id}
+                    className="rounded-xl bg-white text-center shadow-xl"
+                  >
+                    <img
+                      src={bookmark.images}
+                      alt={bookmark.name}
+                      className="h-36 w-auto rounded-t-xl object-cover"
+                    />
+                    <div className="p-2 text-center">
+                      <h3 className="text-sm font-semibold">{bookmark.name}</h3>
+                    </div>
+                  </article>
+                ))}
+              </div>
+            </div>
           )}
         </div>
       </div>
@@ -37,4 +42,4 @@ function Bookmarked() {
   );
 }
 
-export default Bookmarked;
+export default BookmarkedPage;
