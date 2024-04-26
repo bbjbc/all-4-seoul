921c2b6eb49dd3090dab8c2e7630f5c82f59db64
bbjbc <jobc4550@gmail.com>
2024-04-26 11:30:39 +0900

    feat: 뒤로가기 버튼 컴포넌트화

--- frontend/src/components/detail/main/detail-layout.jsx
+++ frontend/src/components/detail/main/detail-layout.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 
 import propTypes from 'prop-types';
-import { IoArrowBack } from 'react-icons/io5';
+import HandleBackButton from '../../button/handle-back-button';
 
 function DetailLayout({ children }) {
-  const navigation = useNavigate();
-
   const { ref, inView } = useInView({
     threshold: 0.5,
     triggerOnce: true,
   });
 
-  const handleBack = () => {
-    navigation('/list');
-  };
-
   return (
     <div className="flex h-screen" ref={ref}>
       <div className={`relative flex-grow ${inView ? 'animate-slidein' : ''}`}>
-        <div className="absolute left-32 top-20 z-50">
-          <button
-            className="text-md flex flex-row rounded-lg px-2 py-1 text-white transition-all duration-200 ease-in-out hover:bg-teal-700"
-            onClick={handleBack}
-          >
-            <IoArrowBack size={20} />
-            뒤로가기
-          </button>
-        </div>
+        <HandleBackButton path="list" />
         {children}
       </div>
     </div>
