0ebc362339c0192d47f67336a9f0ba6d8a3ae78d
bbjbc <jobc4550@gmail.com>
2024-04-26 23:34:16 +0900

    refactor: 내 정보 페이지 수정

--- frontend/src/components/mypage/my-info.jsx
+++ frontend/src/components/mypage/my-info.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function MyInfo() {
-  return (
-    <div className="relative mt-5 flex h-screen justify-center overflow-hidden">
-      <h2>마이페이지 기본 정보</h2>
-    </div>
-  );
+  return <p>내 정보</p>;
 }
 
 export default MyInfo;
