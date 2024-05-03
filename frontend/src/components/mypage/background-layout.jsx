d882b8c3471d6d8285489023a06343a644b69644
bbjbc <jobc4550@gmail.com>
2024-05-03 17:10:07 +0900

    refactor: 마이페이지 백그라운드 이미지 변경 및 뒤로 가기 버튼 제거

--- frontend/src/components/mypage/background-layout.jsx
+++ frontend/src/components/mypage/background-layout.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 
-import { useLocation } from 'react-router-dom';
-
 import propTypes from 'prop-types';
 
-import logo from '../../assets/올포서울로고.jpg';
-import HandleBackButton from '../button/handle-back-button';
+import logo from '../../assets/seoul.jpg';
 
 function BackgroundLayout({ children }) {
-  const location = useLocation();
-
-  const isMypage = location.pathname === '/mypage';
-
   return (
     <>
-      {!isMypage && <HandleBackButton path="mypage" />}
       <div className="relative flex h-screen justify-center overflow-hidden">
         <img
-          className="absolute inset-0 z-0 h-full w-full object-cover opacity-10"
+          className="absolute inset-0 z-0 h-full w-full object-cover opacity-60"
           src={logo}
           alt="logo"
         />
