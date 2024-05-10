c1e2244513f43d60ec46cd55af66872b37365388
jae woo Jung <awfjol2008@naver.com>
2024-05-10 16:04:42 +0900

    refactor: Place 관련 API 요청 URL 변경

--- backend/src/main/java/com/capstone/all4seoul/place/controller/PlaceController.java
+++ backend/src/main/java/com/capstone/all4seoul/place/controller/PlaceController.java
@@ -11,19 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/places")
 public class PlaceController {
     private final PlaceService placeService;
 
     /**
      * 장소 단건 조회
      */
-    @GetMapping("/places/{placeId}")
+    @GetMapping("/{placeId}")
     public DetailPlaceResponse getPlace(@PathVariable Long placeId) {
         return placeService.findById(placeId);
     }
@@ -31,7 +33,7 @@ public class PlaceController {
     /**
      * 전체 장소 조회
      */
-    @GetMapping("/places")
+    @GetMapping("")
     public List<DetailPlaceResponse> getAllPlaces() {
         return placeService.findAll();
     }
@@ -39,7 +41,7 @@ public class PlaceController {
     /**
      * 장소 이름으로 장소 목록 조회
      */
-    @GetMapping("/places/{name}")
+    @GetMapping("/{name}")
     public List<DetailPlaceResponse> getPlacesByName(@PathVariable String name) {
         return placeService.findPlacesByName(name);
     }
@@ -47,12 +49,12 @@ public class PlaceController {
     /**
      * 구글 API 장소 검색
      */
-    @PostMapping("/places/google")
+    @PostMapping("/google")
     public PlaceSearchResponseByGoogle searchPlacesByGoogle(@RequestBody PlaceSearchRequest placeSearchRequest) {
         return PlaceServiceByExternalApi.searchPlaces(placeSearchRequest);
     }
 
-    @GetMapping("/places/seoulData")
+    @GetMapping("/seoulData")
     public PlaceSearchResponseBySeoulDataApi searchPlacesBySeoulDataApi() {
         return PlaceServiceByExternalApi.searchPlacesBySeoulDataApi();
     }
