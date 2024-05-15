1c5314bfd1967b37cf28df4a33ba72bdd46b306d
fbehddn <tom0611@naver.com>
2024-05-15 16:22:18 +0900

    feat: 서버에서 쿠키 생성 로직 제거

--- backend/src/main/java/com/capstone/all4seoul/login/controller/LoginController.java
+++ backend/src/main/java/com/capstone/all4seoul/login/controller/LoginController.java
@@ -3,9 +3,7 @@ package com.capstone.all4seoul.login.controller;
 import com.capstone.all4seoul.login.dto.request.LoginRequest;
 import com.capstone.all4seoul.login.service.LoginService;
 import com.capstone.all4seoul.user.domain.User;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -31,14 +28,8 @@ public class LoginController {
         }
         HttpSession session = request.getSession(true);
 
-        session.setAttribute("loginUser", loginUser.getId());
+        session.setAttribute("userId", loginUser.getId());
         session.setMaxInactiveInterval(1800);
-
-//        Cookie sessionCookie = new Cookie("theCookieMadeByMe", session.getId());
-//        sessionCookie.setMaxAge(1800); // 세션 만료 시간 설정 (초 단위)
-//        sessionCookie.setPath("/"); // 쿠키의 경로 설정
-
-//        response.addCookie(sessionCookie);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("로그인 성공");
     }
