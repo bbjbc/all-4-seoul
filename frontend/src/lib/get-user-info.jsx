08b4ca220e811a2f8e068d140420efba2029e31c
fbehddn <tom0611@naver.com>
2024-05-15 16:23:15 +0900

    fix: 클라이언트에서 user-info API를 호출하도록 변경

--- frontend/src/lib/get-user-info.jsx
+++ frontend/src/lib/get-user-info.jsx
@@ -1,18 +1,9 @@
 import axios from 'axios';
 
-export async function getUserInfo(userId) {
-  const sessionId = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith('sessionId='));
-  if (!sessionId) {
-    return null;
-  }
-
+export async function getUserInfo() {
   try {
-    const response = await axios.get(`http://localhost:8080/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${sessionId.split('=')[1]}`,
-      },
+    const response = await axios.get('http://localhost:8080/user-info', {
+      withCredentials: true // 쿠키를 자동으로 포함하여 요청합니다.
     });
     return response.data;
   } catch (error) {
