6beef200bdf6392d94c6a8ad9379c15acceff989
bbjbc <jobc4550@gmail.com>
2024-05-27 17:53:27 +0900

    refactor: 시트 오픈 버튼 삭제 및 추천 컨텐츠 컴포넌트 삽입

--- frontend/src/components/recommend/right-sheet.jsx
+++ frontend/src/components/recommend/right-sheet.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import propTypes from 'prop-types';
 
-import { IoMdClose, IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { IoMdClose, IoIosArrowForward } from 'react-icons/io';
+import RecommendContents from './recommend-contents';
 
 function RightSheet({ isOpen, onClose }) {
   return (
@@ -13,7 +14,7 @@ function RightSheet({ isOpen, onClose }) {
         <h2 className="text-lg font-semibold">회원님의 Today&apos;s PICK!</h2>
         <button
           onClick={onClose}
-          className="hover:animate-swing text-gray-600 hover:text-gray-800"
+          className="text-gray-600 hover:animate-swing hover:text-gray-800"
         >
           <IoMdClose size={26} />
         </button>
@@ -23,17 +24,16 @@ function RightSheet({ isOpen, onClose }) {
           회원님을 고려한 추천 리스트입니다. <br />
           오늘의 추천을 확인해보세요!
         </p>
+        <RecommendContents />
 
-        <button
-          className="absolute right-full top-1/2 rounded-l-xl bg-amber-500 px-1 py-2 text-white shadow-lg hover:bg-amber-400"
-          onClick={onClose}
-        >
-          {isOpen ? (
+        {isOpen && (
+          <button
+            className="absolute right-full top-1/2 rounded-l-xl bg-amber-500 px-1 py-2 text-white shadow-lg hover:bg-amber-400"
+            onClick={onClose}
+          >
             <IoIosArrowForward size={26} />
-          ) : (
-            <IoIosArrowBack size={26} />
-          )}
-        </button>
+          </button>
+        )}
       </div>
     </div>
   );
