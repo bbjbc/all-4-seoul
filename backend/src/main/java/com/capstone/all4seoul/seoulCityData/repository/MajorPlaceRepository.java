22d0ccf996b11338afbaa11322f50f22c1a0e501
jae woo Jung <awfjol2008@gmail.com>
2024-05-28 16:41:14 +0900

    feat: MajorPlace 리포지토리 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/repository/MajorPlaceRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.all4seoul.seoulCityData.repository;
+
+import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MajorPlaceRepository extends JpaRepository<MajorPlace, Long> {
+}
