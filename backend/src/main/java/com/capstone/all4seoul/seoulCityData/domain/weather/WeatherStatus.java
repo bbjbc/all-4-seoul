f966da8251da6e6ee379a4a6363fd71978dfdb8e
jae woo Jung <awfjol2008@gmail.com>
2024-05-30 22:17:54 +0900

    feat: WeatherForecast 클래스 WeatherStatus 클래스에 이너 스태틱 클래스로 포함

--- backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/weather/WeatherStatus.java
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/weather/WeatherStatus.java
@@ -1,5 +1,18 @@
 package com.capstone.all4seoul.seoulCityData.domain.weather;
 
+import com.capstone.all4seoul.place.dto.response.externalApi.PlaceSearchResponseBySeoulDataApi;
+import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,9 +20,16 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Getter
+@Table(name = "weather_statuses")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WeatherStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weather_status_id")
+    private Long id;
+
     private String time;
 
     private String temperature;
@@ -50,55 +70,84 @@ public class WeatherStatus {
 
     private String airMessage;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "major_place_id")
+    private MajorPlace majorPlace;
+
+    @OneToMany(mappedBy = "weatherStatus", cascade = CascadeType.ALL)
     private List<WeatherForecast> weatherForecasts = new ArrayList<>();
 
     public static WeatherStatus createWeatherStatus(
-            String weatherTime,
-            String temperature,
-            String sensibleTemperature,
-            String maximumTemperature,
-            String minimumTemperature,
-            String humidity,
-            String precipitation,
-            String precipitationType,
-            String precipitationMessage,
-            String uvIndexLevel,
-            String uvIndex,
-            String uvMessage,
-            String pm25Index,
-            String pm25,
-            String pm10Index,
-            String pm10,
-            String airIndex,
-            String airIndexMvl,
-            String airIndexMain,
-            String airMessage,
+            PlaceSearchResponseBySeoulDataApi.CityData.WeatherStatus fetchedWeatherStatus,
             List<WeatherForecast> weatherForecasts
     ) {
         WeatherStatus weatherStatus = new WeatherStatus();
 
-        weatherStatus.time = weatherTime;
-        weatherStatus.temperature = temperature;
-        weatherStatus.sensibleTemperature = sensibleTemperature;
-        weatherStatus.maximumTemperature = maximumTemperature;
-        weatherStatus.minimumTemperature = minimumTemperature;
-        weatherStatus.humidity = humidity;
-        weatherStatus.precipitation = precipitation;
-        weatherStatus.precipitationType = precipitationType;
-        weatherStatus.precipitationMessage = precipitationMessage;
-        weatherStatus.uvIndexLevel = uvIndexLevel;
-        weatherStatus.uvIndex = uvIndex;
-        weatherStatus.uvMessage = uvMessage;
-        weatherStatus.pm25Index = pm25Index;
-        weatherStatus.pm25 = pm25;
-        weatherStatus.pm10Index = pm10Index;
-        weatherStatus.pm10 = pm10;
-        weatherStatus.airIndex = airIndex;
-        weatherStatus.airIndexMvl = airIndexMvl;
-        weatherStatus.airIndexMain = airIndexMain;
-        weatherStatus.airMessage = airMessage;
+        weatherStatus.time = fetchedWeatherStatus.getTime();
+        weatherStatus.temperature = fetchedWeatherStatus.getTemperature();
+        weatherStatus.sensibleTemperature = fetchedWeatherStatus.getSensibleTemperature();
+        weatherStatus.maximumTemperature = fetchedWeatherStatus.getMaximumTemperature();
+        weatherStatus.minimumTemperature = fetchedWeatherStatus.getMinimumTemperature();
+        weatherStatus.humidity = fetchedWeatherStatus.getHumidity();
+        weatherStatus.precipitation = fetchedWeatherStatus.getPrecipitation();
+        weatherStatus.precipitationType = fetchedWeatherStatus.getPrecipitationType();
+        weatherStatus.precipitationMessage = fetchedWeatherStatus.getPrecipitationMessage();
+        weatherStatus.uvIndexLevel = fetchedWeatherStatus.getUvIndexLevel();
+        weatherStatus.uvIndex = fetchedWeatherStatus.getUvIndex();
+        weatherStatus.uvMessage = fetchedWeatherStatus.getUvMessage();
+        weatherStatus.pm25Index = fetchedWeatherStatus.getPm25Index();
+        weatherStatus.pm25 = fetchedWeatherStatus.getPm25();
+        weatherStatus.pm10Index = fetchedWeatherStatus.getPm10Index();
+        weatherStatus.pm10 = fetchedWeatherStatus.getPm10();
+        weatherStatus.airIndex = fetchedWeatherStatus.getAirIndex();
+        weatherStatus.airIndexMvl = fetchedWeatherStatus.getAirIndexMvl();
+        weatherStatus.airIndexMain = fetchedWeatherStatus.getAirIndexMain();
+        weatherStatus.airMessage = fetchedWeatherStatus.getAirMessage();
         weatherStatus.weatherForecasts = weatherForecasts;
 
         return weatherStatus;
     }
+
+    @Entity
+    @Getter
+    @Table(name = "weather_forecasts")
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class WeatherForecast {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "weather_forecast_id")
+        private Long id;
+
+        private String dayTime;
+
+        private String temperature24Hour;
+
+        private String precipitation;
+
+        private String precipitationType;
+
+        private String rainChance;
+
+        private String skyStatus;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "weather_status")
+        private WeatherStatus weatherStatus;
+
+        public static WeatherForecast createWeatherForecast(
+                PlaceSearchResponseBySeoulDataApi.CityData.WeatherStatus.Forecast24Hour fetchedForecast24Hour
+        ) {
+            WeatherForecast weatherForecast = new WeatherForecast();
+
+            weatherForecast.dayTime = fetchedForecast24Hour.getDayTime();
+            weatherForecast.temperature24Hour = fetchedForecast24Hour.getTemperature24Hour();
+            weatherForecast.precipitation = fetchedForecast24Hour.getPrecipitation();
+            weatherForecast.precipitationType = fetchedForecast24Hour.getPrecipitationType();
+            weatherForecast.rainChance = fetchedForecast24Hour.getRainChance();
+            weatherForecast.skyStatus = fetchedForecast24Hour.getSkyStatus();
+
+            return weatherForecast;
+        }
+    }
+
 }
