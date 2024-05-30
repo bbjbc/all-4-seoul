f67fc3f9975b627636cc739aa81742584c0f4587
jae woo Jung <awfjol2008@gmail.com>
2024-05-30 22:28:29 +0900

    feat: MajorPlace 응답 DTO 구현

--- backend/src/main/java/com/capstone/all4seoul/place/dto/response/MajorPlaceResponse.java
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/response/MajorPlaceResponse.java
@@ -1,22 +1,45 @@
 package com.capstone.all4seoul.place.dto.response;
 
 import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
+import com.capstone.all4seoul.seoulCityData.domain.charger.ChargerStation;
+import com.capstone.all4seoul.seoulCityData.domain.event.AdjacentEvent;
+import com.capstone.all4seoul.seoulCityData.domain.parkingLot.ParkingLot;
+import com.capstone.all4seoul.seoulCityData.domain.population.LivePopulationStatus;
+import com.capstone.all4seoul.seoulCityData.domain.weather.WeatherStatus;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MajorPlaceResponse {
-    private String category;
     private String areaName;
+
+    private String areaCode;
+
+    private List<LivePopulationStatus> livePopulationStatus;
+
+    private List<ParkingLot> parkingLot;
+
+    private List<ChargerStation> chargerStation;
+
+    private List<WeatherStatus> weatherStatus;
+
+    private List<AdjacentEvent> adjacentEvent;
 
 
     public static MajorPlaceResponse of(MajorPlace majorPlace) {
         MajorPlaceResponse majorPlaceResponse = new MajorPlaceResponse();
 
-        majorPlaceResponse.category = majorPlace.getCategory();
         majorPlaceResponse.areaName = majorPlace.getAreaName();
+        majorPlaceResponse.areaCode = majorPlace.getAreaCode();
+        majorPlaceResponse.livePopulationStatus = majorPlace.getLivePopulationStatus();
+        majorPlaceResponse.parkingLot = majorPlace.getParkingLot();
+        majorPlaceResponse.chargerStation = majorPlace.getChargerStation();
+        majorPlaceResponse.weatherStatus = majorPlace.getWeatherStatus();
+        majorPlaceResponse.adjacentEvent = majorPlace.getAdjacentEvent();
 
         return majorPlaceResponse;
     }
