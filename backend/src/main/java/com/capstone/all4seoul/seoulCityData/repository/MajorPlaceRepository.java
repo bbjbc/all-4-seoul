388eb1a9d90e383e039bf8c907b173e8ee32db22
jae woo Jung <awfjol2008@gmail.com>
2024-05-30 22:28:15 +0900

    feat: MajorPlaceRepository 디렉토리 이동 및 개편

--- backend/src/main/java/com/capstone/all4seoul/seoulCityData/repository/MajorPlaceRepository.java
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/repository/MajorPlaceRepository.java
@@ -2,6 +2,17 @@ package com.capstone.all4seoul.seoulCityData.repository;
 
 import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface MajorPlaceRepository extends JpaRepository<MajorPlace, Long> {
+    Optional<MajorPlace> findFirstByAreaNameContains(String areaName);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE MajorPlace m SET m.latest = FALSE WHERE m.latest = TRUE")
+    void updateLatestFalseIfTrue();
 }
