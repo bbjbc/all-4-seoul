cf4147a0376a24b5fb67bdc2d291727a91859ab7
jae woo Jung <awfjol2008@gmail.com>
2024-05-30 22:25:18 +0900

    feat: AdjacentEvent 클래스 엔티티화

--- backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/event/AdjacentEvent.java
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/event/AdjacentEvent.java
@@ -1,12 +1,30 @@
 package com.capstone.all4seoul.seoulCityData.domain.event;
 
+import com.capstone.all4seoul.place.dto.response.externalApi.PlaceSearchResponseBySeoulDataApi;
+import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@Table(name = "adjacent_events")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AdjacentEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "adjacent_event_id")
+    private Long id;
+
     private String name;
 
     private String period;
@@ -25,28 +43,24 @@ public class AdjacentEvent {
 
     private String etcDetail;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "major_place_id")
+    private MajorPlace majorPlace;
+
     public static AdjacentEvent createAdjacentEvent(
-            String name,
-            String period,
-            String place,
-            String longitude,
-            String latitude,
-            String payYn,
-            String thumbnail,
-            String url,
-            String etcDetail
+            PlaceSearchResponseBySeoulDataApi.CityData.AdjacentEvent fetchedAdjacentEvent
     ) {
         AdjacentEvent adjacentEvent = new AdjacentEvent();
 
-        adjacentEvent.name = name;
-        adjacentEvent.period = period;
-        adjacentEvent.place = place;
-        adjacentEvent.longitude = longitude;
-        adjacentEvent.latitude = latitude;
-        adjacentEvent.payYn = payYn;
-        adjacentEvent.thumbnail = thumbnail;
-        adjacentEvent.url = url;
-        adjacentEvent.etcDetail = etcDetail;
+        adjacentEvent.name = fetchedAdjacentEvent.getName();
+        adjacentEvent.period = fetchedAdjacentEvent.getPeriod();
+        adjacentEvent.place = fetchedAdjacentEvent.getPlace();
+        adjacentEvent.longitude = fetchedAdjacentEvent.getLongitude();
+        adjacentEvent.latitude = fetchedAdjacentEvent.getLatitude();
+        adjacentEvent.payYn = fetchedAdjacentEvent.getPayYn();
+        adjacentEvent.thumbnail = fetchedAdjacentEvent.getThumbnail();
+        adjacentEvent.url = fetchedAdjacentEvent.getUrl();
+        adjacentEvent.etcDetail = fetchedAdjacentEvent.getEtcDetail();
 
         return adjacentEvent;
     }
