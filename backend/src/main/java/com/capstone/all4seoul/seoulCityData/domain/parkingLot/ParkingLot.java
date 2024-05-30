ac554aed5cca3d0d6d8fc51064e7e6808520926f
jae woo Jung <awfjol2008@gmail.com>
2024-05-30 22:24:50 +0900

    feat: ParkingLot 클래스 엔티티화

--- backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/parkingLot/ParkingLot.java
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/domain/parkingLot/ParkingLot.java
@@ -1,12 +1,30 @@
 package com.capstone.all4seoul.seoulCityData.domain.parkingLot;
 
+import com.capstone.all4seoul.place.dto.response.externalApi.PlaceSearchResponseBySeoulDataApi;
+import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@Table(name = "parking_lots")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ParkingLot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "parking_lot_id")
+    private Long id;
+
     private String name;
 
     private String code;
@@ -37,40 +55,30 @@ public class ParkingLot {
 
     private String latitude;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "major_place_id")
+    private MajorPlace majorPlace;
+
     public static ParkingLot createParkingLot(
-            String name,
-            String code,
-            String capacity,
-            String currentParkingCount,
-            String currentParkingTime,
-            String currentParkingYn,
-            String payYn,
-            String rates,
-            String timeRates,
-            String addRates,
-            String addTimeRates,
-            String address,
-            String roadAddr,
-            String longitude,
-            String latitude
+            PlaceSearchResponseBySeoulDataApi.CityData.ParkingLot fetchedParkingLot
     ) {
         ParkingLot parkingLot = new ParkingLot();
 
-        parkingLot.name = name;
-        parkingLot.code = code;
-        parkingLot.capacity = capacity;
-        parkingLot.currentParkingCount = currentParkingCount;
-        parkingLot.currentParkingTime = currentParkingTime;
-        parkingLot.currentParkingYn = currentParkingYn;
-        parkingLot.payYn = payYn;
-        parkingLot.rates = rates;
-        parkingLot.timeRates = timeRates;
-        parkingLot.addRates = addRates;
-        parkingLot.addTimeRates = addTimeRates;
-        parkingLot.address = address;
-        parkingLot.roadAddr = roadAddr;
-        parkingLot.longitude = longitude;
-        parkingLot.latitude = latitude;
+        parkingLot.name = fetchedParkingLot.getName();
+        parkingLot.code = fetchedParkingLot.getCode();
+        parkingLot.capacity = fetchedParkingLot.getCapacity();
+        parkingLot.currentParkingCount = fetchedParkingLot.getCurrentParkingCount();
+        parkingLot.currentParkingTime = fetchedParkingLot.getCurrentParkingTime();
+        parkingLot.currentParkingYn = fetchedParkingLot.getCurrentParkingYn();
+        parkingLot.payYn = fetchedParkingLot.getPayYn();
+        parkingLot.rates = fetchedParkingLot.getRates();
+        parkingLot.timeRates = fetchedParkingLot.getTimeRates();
+        parkingLot.addRates = fetchedParkingLot.getAddRates();
+        parkingLot.addTimeRates = fetchedParkingLot.getAddTimeRates();
+        parkingLot.address = fetchedParkingLot.getAddress();
+        parkingLot.roadAddr = fetchedParkingLot.getRoadAddr();
+        parkingLot.longitude = fetchedParkingLot.getLongitude();
+        parkingLot.latitude = fetchedParkingLot.getLatitude();
 
         return parkingLot;
     }
