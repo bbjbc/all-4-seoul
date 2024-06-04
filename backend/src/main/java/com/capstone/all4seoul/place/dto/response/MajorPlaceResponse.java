14eed5b131a9c40dd476d27c65bd5fd9de3f360c
jae woo Jung <awfjol2008@gmail.com>
2024-06-04 11:43:44 +0900

    fix: 응답 무한 루프 에러 해결을 위해 MajorPlaceResponse의 모든 필드를 DTO로 변경

--- backend/src/main/java/com/capstone/all4seoul/place/dto/response/MajorPlaceResponse.java
+++ backend/src/main/java/com/capstone/all4seoul/place/dto/response/MajorPlaceResponse.java
@@ -1,11 +1,11 @@
 package com.capstone.all4seoul.place.dto.response;
 
 import com.capstone.all4seoul.seoulCityData.domain.MajorPlace;
-import com.capstone.all4seoul.seoulCityData.domain.charger.ChargerStation;
-import com.capstone.all4seoul.seoulCityData.domain.event.AdjacentEvent;
-import com.capstone.all4seoul.seoulCityData.domain.parkingLot.ParkingLot;
-import com.capstone.all4seoul.seoulCityData.domain.population.LivePopulationStatus;
-import com.capstone.all4seoul.seoulCityData.domain.weather.WeatherStatus;
+import com.capstone.all4seoul.seoulCityData.dto.response.AdjacentEventResponse;
+import com.capstone.all4seoul.seoulCityData.dto.response.ChargerStationResponse;
+import com.capstone.all4seoul.seoulCityData.dto.response.LivePopulationStatusResponse;
+import com.capstone.all4seoul.seoulCityData.dto.response.ParkingLotResponse;
+import com.capstone.all4seoul.seoulCityData.dto.response.WeatherStatusResponse;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,27 +19,41 @@ public class MajorPlaceResponse {
 
     private String areaCode;
 
-    private List<LivePopulationStatus> livePopulationStatuses; // DTO 만들어야댐
+    private List<LivePopulationStatusResponse> livePopulationStatuses;
 
-    private List<ParkingLot> parkingLots;
+    private List<ParkingLotResponse> parkingLots;
 
-    private List<ChargerStation> chargerStations;
+    private List<ChargerStationResponse> chargerStations;
 
-    private List<WeatherStatus> weatherStatuses;
+    private List<WeatherStatusResponse> weatherStatuses;
 
-    private List<AdjacentEvent> adjacentEvents;
-
+    private List<AdjacentEventResponse> adjacentEvents;
 
     public static MajorPlaceResponse of(MajorPlace majorPlace) {
         MajorPlaceResponse majorPlaceResponse = new MajorPlaceResponse();
 
         majorPlaceResponse.areaName = majorPlace.getAreaName();
         majorPlaceResponse.areaCode = majorPlace.getAreaCode();
-        majorPlaceResponse.livePopulationStatuses = majorPlace.getLivePopulationStatuses();
-        majorPlaceResponse.parkingLots = majorPlace.getParkingLots();
-        majorPlaceResponse.chargerStations = majorPlace.getChargerStations();
-        majorPlaceResponse.weatherStatuses = majorPlace.getWeatherStatuses();
-        majorPlaceResponse.adjacentEvents = majorPlace.getAdjacentEvents();
+        majorPlaceResponse.livePopulationStatuses = majorPlace.getLivePopulationStatuses()
+                .stream()
+                .map(LivePopulationStatusResponse::of)
+                .toList();
+        majorPlaceResponse.parkingLots = majorPlace.getParkingLots()
+                .stream()
+                .map(ParkingLotResponse::of)
+                .toList();
+        majorPlaceResponse.chargerStations = majorPlace.getChargerStations()
+                .stream()
+                .map(ChargerStationResponse::of)
+                .toList();
+        majorPlaceResponse.weatherStatuses = majorPlace.getWeatherStatuses()
+                .stream()
+                .map(WeatherStatusResponse::of)
+                .toList();
+        majorPlaceResponse.adjacentEvents = majorPlace.getAdjacentEvents()
+                .stream()
+                .map(AdjacentEventResponse::of)
+                .toList();
 
         return majorPlaceResponse;
     }
