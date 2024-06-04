6d07e3cdc5fdbd09efb0996c65a5a3cf9f0a6b2b
bbjbc <jobc4550@gmail.com>
2024-06-05 03:11:12 +0900

    feat: 인구 정보 데이터 삽입

--- frontend/src/components/detail/population/population-info.jsx
+++ frontend/src/components/detail/population/population-info.jsx
@@ -3,21 +3,27 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 import BarGraph from './bar-graph';
-import { generateTime } from '../dummy-data';
+import {
+  generateTime,
+  getDifferenceWithCurrentHour,
+  getMaxPopulationTimeAndMaxPopulation,
+} from './calc-func';
 import populationInfo from '../../../assets/detail-background/info.jpg';
 import { IoMdTimer } from 'react-icons/io';
-import { RxCountdownTimer } from 'react-icons/rx';
 
-function PopulationInfo({ populationRef }) {
-  const {
-    timeData,
-    futureMaxPopulation,
-    futureMaxHour,
-    pastMaxPopulation,
-    pastMaxHour,
-    futureHourDiff,
-    pastHourDiff,
-  } = generateTime();
+function PopulationInfo({ populationRef, data = [] }) {
+  const timeData = generateTime(data);
+  const { maxPopulationTime, maxPopulation, maxCongestLevel } =
+    getMaxPopulationTimeAndMaxPopulation(data) || {
+      maxPopulationTime: '데이터 없음',
+      maxPopulation: '데이터 없음',
+      maxCongestLevel: '데이터 없음',
+    };
+
+  const currentTime =
+    data.length > 0 && data[0].populationTime
+      ? data[0].populationTime
+      : '데이터 없음';
 
   return (
     <main className="relative h-full w-full" ref={populationRef}>
@@ -28,14 +34,13 @@ function PopulationInfo({ populationRef }) {
       />
       <section className="flex items-center justify-center gap-10 pl-40 pr-16">
         <article className="relative top-32 z-10 h-auto w-2/3 rounded-lg bg-white p-5 shadow-lg">
-          <p className="text-sm text-gray-700">
-            ※ {new Date().toLocaleTimeString()} 기준
-          </p>
+          <p className="text-sm text-gray-700">※ {currentTime} 기준</p>
           <h3 className="my-5 p-2 text-2xl font-extrabold text-indigo-800">
             실시간 인구 및 혼잡도 추이 전망
           </h3>
           <BarGraph data={timeData} />
         </article>
+
         <article className="relative top-32 z-10 h-auto w-1/3 rounded-lg bg-white p-5 shadow-lg">
           <div className="mb-6 rounded-lg p-3 transition-all duration-200 ease-in-out hover:bg-blue-100">
             <h1 className="mb-2 flex flex-row justify-center gap-3 text-2xl font-bold text-gray-800">
@@ -44,34 +49,20 @@ function PopulationInfo({ populationRef }) {
             </h1>
             <p className="text-left">
               <span className="text-lg text-blue-700">
-                {futureMaxHour}({futureHourDiff}
-                시간 후)
+                {maxPopulationTime}시(
+                {getDifferenceWithCurrentHour(data)} 시간 후)
               </span>
               <span>
                 에 인구가 제일 많고 혼잡도도 가장 높을 것으로 예상돼요.
-                혼잡정도는 붐빔일 것으로 예상돼요.
+                혼잡정도는{' '}
+                <span className="text-lg font-bold text-orange-800">
+                  {maxCongestLevel}
+                </span>
+                일 것으로 예상돼요.
               </span>
             </p>
             <p className="p-3 text-base text-gray-600">
-              인구 수는 {futureMaxPopulation}명으로 예측돼요.
-            </p>
-          </div>
-          <div className="rounded-lg p-3 transition-all duration-200 ease-in-out hover:bg-orange-100">
-            <h1 className="mb-2 flex flex-row justify-center gap-3 text-2xl font-bold text-gray-800">
-              <RxCountdownTimer size={30} />
-              <span>지난 12시간 추이</span>
-            </h1>
-            <p className="text-left">
-              <span className="text-lg text-orange-700">
-                {pastMaxHour}({pastHourDiff}시간 전)
-              </span>
-              <span>
-                에 인구가 제일 많았고 혼잡도도 가장 높았어요. 혼잡정도는 약간
-                붐빔이었어요.
-              </span>
-            </p>
-            <p className="p-3 text-base text-gray-600">
-              최대 인구수는 {pastMaxPopulation}명이었어요.
+              인구 수는 {maxPopulation}명으로 예측돼요.
             </p>
           </div>
         </article>
@@ -82,6 +73,7 @@ function PopulationInfo({ populationRef }) {
 
 PopulationInfo.propTypes = {
   populationRef: propTypes.object.isRequired,
+  data: propTypes.array,
 };
 
 export default PopulationInfo;
