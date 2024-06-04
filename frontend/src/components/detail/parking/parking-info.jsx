5d7a4ceb992d4eb1cf8f3b72a42294f2212a371a
bbjbc <jobc4550@gmail.com>
2024-06-05 00:25:38 +0900

    refactor: css와 관련한 로직 수정 (키 값 수정은 추후에 다시 봐야함)

--- frontend/src/components/detail/parking/parking-info.jsx
+++ frontend/src/components/detail/parking/parking-info.jsx
@@ -41,54 +41,80 @@ function ParkingInfo({
 
           <article className="h-3/4 overflow-y-auto rounded-lg bg-zinc-100 p-2">
             {parkingLots.map((parking) => (
-              <div key={parking.code} className="m-2">
+              <div key={`parking-${parking.code}`} className="m-2">
                 <button
-                  className={`flex w-full items-center justify-between rounded-lg bg-white p-4 shadow-md transition-all duration-300 ${parking.capacity - parking.currentParkingCount > 0 ? 'hover:bg-blue-100' : 'hover:bg-red-100'}`}
-                  onClick={() => handleAccordion(parking.code)}
+                  className="flex w-full items-center justify-between rounded-lg bg-white p-4 shadow-md transition-all duration-300 hover:bg-blue-100"
+                  onClick={() => handleAccordion(`parking-${parking.code}`)}
                 >
                   <h2 className="text-lg font-semibold">{parking.name}</h2>
-                  <span
-                    className={`font-semibold ${
-                      parking.capacity - parking.currentParkingCount > 0
-                        ? 'text-blue-600'
-                        : 'text-red-600'
-                    }`}
-                  >
-                    {parking.capacity - parking.currentParkingCount > 0
-                      ? '주차 가능'
-                      : '주차 불가'}
-                  </span>
+                  <span className="font-semibold text-blue-600">주차장</span>
                 </button>
 
                 <div
-                  className={`mt-2 overflow-hidden rounded-lg bg-white shadow-md ${parking.capacity - parking.currentParkingCount > 0 ? 'bg-blue-100' : 'bg-red-100'} ${
-                    expandedItems[parking.code]
+                  className={`mt-2 overflow-hidden rounded-lg bg-white shadow-md ${
+                    expandedItems[`parking-${parking.code}`]
                       ? 'max-h-96 animate-expand'
                       : 'max-h-0 animate-collapse'
                   }`}
                 >
                   <div className="p-4">
-                    <p className="mb-2">요금 정보: {parking.rates}</p>
                     <p className="mb-2">
-                      개방시간: {parking.currentParkingTime}
+                      {parking.payYn === 'Y' || parking.payYn === '유료'
+                        ? '유료 주차장'
+                        : '무료 주차장'}
                     </p>
-                    <p className="mb-2">주소: {parking.currentParkingTime}</p>
-                    <p className="mb-2">주차 용량: {parking.address}</p>
-                    <p className="mb-2">시간머시기: {parking.timeRates}</p>
-                    <p className="mb-2">추가 비용: {parking.addRates}</p>
-                    <p className="mb-2">
-                      추가 시간 비용: {parking.addTimeRates}
-                    </p>
+                    {parking.payYn === '무료' || parking.payYn === 'N' ? (
+                      <>
+                        <p className="mb-2 text-blue-500">
+                          무료로 이용하시면 됩니다!
+                        </p>
+                        <p
+                          className={`mb-2 ${
+                            parking.currentParkingYn === 'Y'
+                              ? 'text-blue-500'
+                              : 'text-red-500'
+                          }`}
+                        >
+                          {parking.currentParkingYn === 'Y'
+                            ? '현재 주차 가능합니다'
+                            : '현재 주차 불가능합니다.'}
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        <p className="mb-2">
+                          요금 정보: 시간 당 {parking.timeRates}원
+                        </p>
+                        <p className="mb-2">주소: {parking.address}</p>
+                        <p
+                          className={`mb-2 ${
+                            parking.currentParkingYn === 'Y'
+                              ? 'text-blue-500'
+                              : 'text-red-500'
+                          }`}
+                        >
+                          {parking.currentParkingYn === 'Y'
+                            ? '현재 주차 가능합니다'
+                            : '현재 주차 불가능합니다.'}
+                        </p>
+                        <p className="mb-2">추가 비용: {parking.addRates}</p>
+                        <p className="mb-2">
+                          추가 시간 비용: {parking.addTimeRates}
+                        </p>
+                      </>
+                    )}
                   </div>
                 </div>
               </div>
             ))}
 
-            {chargerStations.map((charger, index) => (
-              <div key={`charger-${index}`} className="m-2">
+            {chargerStations.map((charger) => (
+              <div key={`charger-${charger.stationId}`} className="m-2">
                 <button
                   className="flex w-full items-center justify-between rounded-lg bg-white p-4 shadow-md transition-all duration-300 hover:bg-green-100"
-                  onClick={() => handleAccordion(`charger-${index}`)}
+                  onClick={() =>
+                    handleAccordion(`charger-${charger.stationId}`)
+                  }
                 >
                   <h2 className="text-lg font-semibold">{charger.name}</h2>
                   <span className="font-semibold text-green-600">충전소</span>
@@ -96,27 +122,79 @@ function ParkingInfo({
 
                 <div
                   className={`mt-2 overflow-hidden rounded-lg bg-white shadow-md ${
-                    expandedItems[`charger-${index}`]
+                    expandedItems[`charger-${charger.stationId}`]
                       ? 'max-h-96 animate-expand'
                       : 'max-h-0 animate-collapse'
                   }`}
                 >
-                  <div className="p-4">
-                    <p className="mb-2">주소: {charger.address}</p>
-                    <p className="mb-2">사용 시간: {charger.useTime}</p>
-                    <p className="mb-2">주차 요금: {charger.parkPay}</p>
-                    <p className="mb-2">
-                      제한 여부: {charger.limitYn ? '있음' : '없음'}
-                    </p>
-                    <p className="mb-2">제한 상세: {charger.limitDetail}</p>
-                    <p className="mb-2">종류 상세: {charger.kindDetail}</p>
-                    <ul className="list-inside list-disc">
-                      {charger.chargerDetails.map((c) => (
-                        <li key={c.id}>
-                          ID: {c.chargerId}, 타입: {c.type}, 상태: {c.status}
-                        </li>
-                      ))}
-                    </ul>
+                  <div className="flex p-4">
+                    <div className="w-1/2 text-left">
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">주소: </span>
+                        <span className="text-gray-800">{charger.address}</span>
+                      </p>
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">사용 시간: </span>
+                        <span className="text-gray-800">{charger.useTime}</span>
+                      </p>
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">주차 요금: </span>
+                        <span
+                          className={`${
+                            charger.parkPay === 'Y'
+                              ? 'text-red-600'
+                              : 'text-green-600'
+                          }`}
+                        >
+                          {charger.parkPay === 'Y'
+                            ? '유료입니다.'
+                            : '무료입니다.'}
+                        </span>
+                      </p>
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">제한 여부: </span>
+                        <span className="text-gray-800">
+                          {charger.limitYn ? '있음' : '없음'}
+                        </span>
+                      </p>
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">제한 상세: </span>
+                        <span className="text-gray-800">
+                          {charger.limitDetail === ''
+                            ? '정보 없음'
+                            : charger.limitDetail}
+                        </span>
+                      </p>
+                      <p className="mb-2 font-semibold">
+                        <span className="text-gray-600">종류 상세: </span>
+                        <span className="text-gray-800">
+                          {charger.kindDetail}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="h-full w-1/2 overflow-y-auto border-l border-gray-200 pl-4">
+                      <ul className="list-inside list-disc">
+                        {charger.chargerDetails.map((c) => (
+                          <li key={c.id} className="mb-2">
+                            <span className="font-semibold text-gray-600">
+                              ID:{' '}
+                            </span>
+                            <span className="text-gray-800">{c.chargerId}</span>
+                            ,
+                            <span className="font-semibold text-gray-600">
+                              {' '}
+                              타입:{' '}
+                            </span>
+                            <span className="text-gray-800">{c.type}</span>,
+                            <span className="font-semibold text-gray-600">
+                              {' '}
+                              상태:{' '}
+                            </span>
+                            <span className="text-gray-800">{c.status}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
