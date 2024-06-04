599033b77f48cbe88ab08f165b19b2e3bcf86d52
jae woo Jung <awfjol2008@gmail.com>
2024-06-04 11:42:16 +0900

    feat: 인구 예보 응답 DTO 구현

---
+++ backend/src/main/java/com/capstone/all4seoul/seoulCityData/dto/response/PopulationForecastResponse.java
@@ -0,0 +1,26 @@
+package com.capstone.all4seoul.seoulCityData.dto.response;
+
+import com.capstone.all4seoul.seoulCityData.domain.population.LivePopulationStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PopulationForecastResponse {
+    private String time;
+    private String congestLevel;
+    private String minimumForecastPopulation;
+    private String maximumForecastPopulation;
+
+    public static PopulationForecastResponse of(LivePopulationStatus.PopulationForecast populationForecast) {
+        PopulationForecastResponse response = new PopulationForecastResponse();
+
+        response.time = populationForecast.getTime();
+        response.congestLevel = populationForecast.getCongestLevel();
+        response.minimumForecastPopulation = populationForecast.getMinimumForecastPopulation();
+        response.maximumForecastPopulation = populationForecast.getMaximumForecastPopulation();
+
+        return response;
+    }
+}
