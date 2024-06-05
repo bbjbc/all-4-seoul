f0bc6f82d6857e67d443a667ab366721b044b973
bbjbc <jobc4550@gmail.com>
2024-06-05 19:07:40 +0900

    refactor: 서버 api 불분명으로 인한 서버 연동 삭제 및 불필요한 더미 이미지 삭제

--- frontend/src/state/bookmark-context.jsx
+++ frontend/src/state/bookmark-context.jsx
@@ -1,15 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 
-import axios from 'axios';
-
 import propTypes from 'prop-types';
-
-import cafeImg from '../assets/overlay/cafe.jpg';
-import parkingImg from '../assets/overlay/parking.jpg';
-import gasImg from '../assets/overlay/gas.jpg';
-import foodImg from '../assets/overlay/food.jpg';
-import attractionsImg from '../assets/overlay/attractions.jpg';
-import cultureImg from '../assets/overlay/culture.jpg';
 
 const BookmarkContext = createContext();
 
@@ -18,48 +9,15 @@ export const useBookmark = () => useContext(BookmarkContext);
 export const BookmarkProvider = ({ children }) => {
   const [bookmarks, setBookmarks] = useState([]);
 
-  const images = {
-    cafe: cafeImg,
-    parking: parkingImg,
-    gas: gasImg,
-    food: foodImg,
-    attractions: attractionsImg,
-    culture: cultureImg,
+  const addBookmark = (bookmark) => {
+    setBookmarks([...bookmarks, bookmark]);
   };
 
-  const addBookmark = async (bookmark) => {
-    try {
-      const response = await axios.post(
-        'http://localhost:8080/users/bookmarks',
-        bookmark,
-        {
-          withCredentials: true,
-        },
-      );
-      console.log(response);
-      setBookmarks([...bookmarks, bookmark]);
-    } catch (error) {
-      console.error('북마크를 추가하는 중 오류가 발생했습니다.', error);
-    }
-  };
-
-  const removeBookmark = async (ids) => {
-    try {
-      const response = await axios.delete(
-        'http://localhost:8080/users/bookmarks',
-        {
-          data: { ids },
-          withCredentials: true,
-        },
-      );
-      console.log(response);
-      const idsToRemove = Array.isArray(ids) ? ids : [ids];
-      setBookmarks(
-        bookmarks.filter((bookmark) => !idsToRemove.includes(bookmark.id)),
-      );
-    } catch (error) {
-      console.error('북마크를 삭제하는 중 오류가 발생했습니다.', error);
-    }
+  const removeBookmark = (ids) => {
+    const idsToRemove = Array.isArray(ids) ? ids : [ids];
+    setBookmarks(
+      bookmarks.filter((bookmark) => !idsToRemove.includes(bookmark.id)),
+    );
   };
 
   const getBookmarksByType = (type) => {
@@ -73,7 +31,6 @@ export const BookmarkProvider = ({ children }) => {
         addBookmark,
         removeBookmark,
         getBookmarksByType,
-        images,
       }}
     >
       {children}
