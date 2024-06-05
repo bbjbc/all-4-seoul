7a411c21da739cb4b4f74176d258a0a25c3169b2
bbjbc <jobc4550@gmail.com>
2024-06-05 22:42:25 +0900

    feat: 실시간 남녀 비율 데이터 삽입

--- frontend/src/components/detail/realtime/real-time-population.jsx
+++ frontend/src/components/detail/realtime/real-time-population.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import propTypes from 'prop-types';
 
+import PieGenderGraph from './pie-gender-graph';
 import image1 from '../../../assets/congestion/여유.jpg';
 import image2 from '../../../assets/congestion/보통.jpg';
 import image3 from '../../../assets/congestion/약간붐빔.jpg';
 import image4 from '../../../assets/congestion/붐빔.jpg';
 import populationImg from '../../../assets/detail-background/population.jpg';
+import { IoMdTimer } from 'react-icons/io';
+import { TbMessage2 } from 'react-icons/tb';
+import { generateGenderPieData } from './gender-data';
 
 function RealTimePopulation({ name, realtimeRef, data = [] }) {
   let congestionImage = '';
@@ -40,6 +45,8 @@ function RealTimePopulation({ name, realtimeRef, data = [] }) {
     congestionDescription = '인구 혼잡도 정보를 가져올 수 없습니다.';
   }
 
+  const genderData = generateGenderPieData(data);
+
   return (
     <main ref={realtimeRef} className="relative h-full w-full">
       <img
@@ -49,21 +56,35 @@ function RealTimePopulation({ name, realtimeRef, data = [] }) {
       />
       <section className="z-10 flex min-h-screen">
         <article className="z-10 flex w-6/12 items-center pl-40">
-          <div className="w-96 space-y-6 text-left">
-            {congestionDescription.split('\n').map((line, index) => (
-              <p key={index} className="text-2xl font-semibold text-stone-700">
-                {line}
-              </p>
-            ))}
+          <div className="flex w-96 flex-col items-center justify-center rounded-lg bg-white p-3 text-left shadow-md">
+            {/* 현재 성별에 따른 비율 */}
+            <h1 className="mb-1 flex flex-row justify-center gap-3 text-xl font-bold text-gray-800">
+              <IoMdTimer size={26} />
+              <span>현재 성별에 따른 비율</span>
+            </h1>
+            <div className="border-b border-gray-300">
+              <PieGenderGraph data={genderData} />
+            </div>
+
+            {/* 현재 혼잡도 설명 */}
+            <h1 className="mb-1 flex flex-row justify-center gap-3 pt-4 text-xl font-bold text-gray-800">
+              <TbMessage2 size={26} />
+              <span>현재 혼잡도 설명</span>
+            </h1>
+            <p className="text-md p-3 text-center text-gray-600">
+              {congestionDescription}
+            </p>
           </div>
         </article>
 
         <article className="relative flex flex-1">
           <div className="absolute right-20 top-14 mr-14 mt-12 flex w-full flex-col justify-center gap-7 rounded-lg bg-white p-8 shadow-lg">
-            <h1 className="text-3xl font-bold">
-              {new Date().toLocaleTimeString()} 기준
-            </h1>
-            <p className="font-gmarketbold text-5xl text-lime-500">{name}</p>
+            <div className="flex flex-row justify-center gap-3">
+              <h1 className="text-3xl font-bold">
+                {new Date().toLocaleTimeString()} 기준
+              </h1>
+              <p className="font-gmarketbold text-5xl text-lime-500">{name}</p>
+            </div>
             <img
               src={congestionImage}
               alt={data[0]?.areaCongestLevel || '정보 없음'}
