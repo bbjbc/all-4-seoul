8beb58a7ad12f9db40b9b8767f6fa1748c57f885
bbjbc <jobc4550@gmail.com>
2024-06-06 17:32:09 +0900

    refactor: 추천 데이터 클릭 시 링킹 추가

--- frontend/src/components/recommend/recommend-grid.jsx
+++ frontend/src/components/recommend/recommend-grid.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import propTypes from 'prop-types';
 
 function RecommendGrid({ item }) {
@@ -7,21 +9,25 @@ function RecommendGrid({ item }) {
 
   return (
     <div className="rounded-lg bg-white p-4 shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-      <img
-        src={imageUrl}
-        alt={name}
-        className="mb-4 h-40 w-full rounded-t-lg object-cover transition-transform duration-300 hover:scale-105"
-      />
-      <div className="flex flex-row items-baseline gap-2">
-        <h2 className="mb-2 text-xl font-semibold text-black">{name}</h2>
-        <p className="mb-2 text-sm text-gray-600">{category}</p>
-      </div>
-      <p className="mb-4 text-gray-800">{description}</p>
-      <div className="text-sm text-gray-500">
-        <p>
-          리뷰 개수: {reviews} / 평균 가격: {averagePrice}
-        </p>
-      </div>
+      <Link to={`/list/${name}`}>
+        <img
+          src={imageUrl}
+          alt={name}
+          className="mb-4 h-40 w-full rounded-t-lg object-cover transition-transform duration-300 hover:scale-105"
+        />
+        <div className="flex flex-row items-baseline gap-2">
+          <h2 className="mb-2 text-xl font-semibold text-black hover:text-blue-600">
+            {name}
+          </h2>
+          <p className="mb-2 text-sm text-gray-600">{category}</p>
+        </div>
+        <p className="mb-4 text-gray-800">{description}</p>
+        <div className="text-sm text-gray-500">
+          <p>
+            리뷰 개수: {reviews} / 평균 가격: {averagePrice}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
