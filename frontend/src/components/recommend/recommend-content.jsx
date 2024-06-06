9e9141813fa0c67a47a23351c8fd4a7feeca5065
bbjbc <jobc4550@gmail.com>
2024-06-06 17:27:25 +0900

    refactor: 추천 데이터 훅을 사용하여 코드 리팩토링

--- frontend/src/components/recommend/recommend-content.jsx
+++ frontend/src/components/recommend/recommend-content.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import RecommendGrid from './recommend-grid';
-import { singleData } from './dummy-data';
+import { useRecommendData } from '../../hooks/use-recommend-data';
 
 function RecommendContent() {
+  const shuffledData = useRecommendData();
+  const singleData = shuffledData.length > 0 ? shuffledData[0] : null;
+
   return (
-    <div className="p-3">
-      <RecommendGrid item={singleData[0]} />
+    <div className="flex p-3 text-left">
+      {singleData && <RecommendGrid item={singleData} />}
     </div>
   );
 }
--- frontend/src/components/recommend/recommend-contents.jsx
+++ frontend/src/components/recommend/recommend-contents.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 import RecommendGrid from './recommend-grid';
-import { multipleData } from './dummy-data';
+import { useRecommendData } from '../../hooks/use-recommend-data';
 
 function RecommendContents() {
+  const shuffledData = useRecommendData();
+
   return (
     <div className="m-4 h-3/4 overflow-y-auto rounded-lg bg-gray-100 p-6">
       <h1 className="mb-6 text-xl font-bold">
         All For Seoul&apos;s PICK!{' '}
-        <span className="text-gray-500">({multipleData.length}개)</span>
+        <span className="text-gray-500">(10개 선별)</span>
       </h1>
       <div className="mb-6 flex flex-col gap-6">
-        {multipleData.map((item, index) => (
+        {shuffledData.map((item, index) => (
           <RecommendGrid key={index} item={item} />
         ))}
       </div>
